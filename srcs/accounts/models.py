d478eec33e9a6a5a821592bd6d99ca9e123cdb29
Abdelkader  Aoutem <abdlkadraoutem@gmail.com>
2024-07-27 05:54:42 +0100

    project struct

---
+++ srcs/accounts/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    user_nmae=models.CharField(max_length=50)
+    email=models.EmailField(max_length=254)
---
+++ srcs/accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+urlpatterns = [
+    path('', ),
+    path('login/',), # add a view and name 
+    path('/register/',), #add a view and name 
+]
