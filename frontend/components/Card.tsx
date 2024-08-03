034cf962241889e7de0948cdaeccd731408c0994
Nourdine <88409152+noureddine-ake@users.noreply.github.com>
2024-08-03 04:43:07 +0100

    setup the back ground

---
+++ frontend/components/Card.tsx
@@ -0,0 +1,23 @@
+import './components.css';
+
+function Card() {
+  return (
+    <div
+      style={{
+        background: 'rgba(88, 88, 84, 0.5)',
+        height: '300px',
+        minWidth: '500px',
+        color: 'white',
+        padding: '20px',
+        borderRadius: '10px',
+        width: '50%',
+        zIndex: 2,
+      }}
+    >
+      <h2>Start your own halucinating ping pong journey</h2>
+      <p>welcome to PiPo lorem ipsum ipsum lorem hehe makayn maytgal </p>
+    </div>
+  );
+}
+
+export default Card;
