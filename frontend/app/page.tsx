cbb2aafa9900fd807d000777b4a1bdc4c3f32446
Nourdine <88409152+noureddine-ake@users.noreply.github.com>
2024-08-04 07:52:15 +0100

    setup card content

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,64 +2,53 @@ import Image from 'next/image';
 import styles from './page.module.css';
 import Circle from '@/components/Circle';
 import Card from '@/components/Card';
+import circleData from '@/constants/circleData';
 
 export default function Home() {
   return (
     <div
       style={{
-        display: 'flex',
+        position: 'relative',
         width: '100%',
         height: '100vh',
         overflow: 'hidden',
         backgroundColor: '#13191D',
+        zIndex: 1,
       }}
     >
       <div
         style={{
           position: 'relative',
-          width: '100%',
+          width: '100vw',
           height: '100vh',
           overflow: 'hidden',
-          backgroundColor: '#13191D',
-          zIndex: 1,
         }}
       >
-        <Circle
-          title="circle1"
-          top="-310px"
-          left="-310px"
-          transform={140}
-          w="300px"
-          h="300px"
-        />
-        <Circle
-          title="circle2"
-          top="-150px"
-          left="460px"
-          transform={170}
-          w="90px"
-          h="90px"
-        />
-        <Circle
-          title="circle3"
-          top="500px"
-          left="-500px"
-          transform={200}
-          w="400px"
-          h="400px"
-        />
-      </div>
-      <div
-        style={{
-          height: '100vh',
-          width: '100%',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          zIndex: 2,
-        }}
-      >
-        <Card />
+        {circleData.map((circle, index) => (
+          <Circle
+            key={index}
+            w={circle.w}
+            h={circle.h}
+            top={circle.top}
+            left={circle.left}
+            etop={circle.etop}
+            eleft={circle.eleft}
+            transform={circle.transform}
+            title={circle.title}
+          />
+        ))}
+        <div
+          style={{
+            height: '100vh',
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            zIndex: 2,
+          }}
+        >
+          <Card />
+        </div>
       </div>
     </div>
   );
