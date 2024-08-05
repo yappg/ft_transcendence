bed642a8503dd6b4a873db9a3a0eaacb4ad701d0
Nourdine <88409152+noureddine-ake@users.noreply.github.com>
2024-08-05 03:40:14 +0100

    still just need to add the logo and everything is done

--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -1,14 +1,25 @@
 import './components.css';
 import Racket from '@/public/racket.svg';
+import { MdOutlineLanguage } from 'react-icons/md';
 
 const Card = () => {
   return (
     <div className="card">
       <div className="left">
-        <div className="lang">O</div>
-        <h2>Start your own halucinating ping pong journey</h2>
-        <p>welcome to PiPo lorem ipsum ipsum lorem hehe makayn maytgal </p>
-        <button className="explore">explore</button>
+        <div className="lang-box">
+          <div className="lang">
+            <MdOutlineLanguage />
+          </div>
+        </div>
+        <div className="title">
+          <h2>Start your own halucinating ping pong journey</h2>
+        </div>
+        <div className="paragraph">
+          <p>welcome to PiPo lorem ipsum ipsum lorem hehe makayn maytgal </p>
+        </div>
+        <button className="explore">
+          <a href="/home">Explore</a>
+        </button>
       </div>
       <div className="right">
         <Racket />
