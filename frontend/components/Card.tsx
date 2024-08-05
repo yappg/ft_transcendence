344b9ffc35407451aed9765e7e346901947db2c6
Nourdine <88409152+noureddine-ake@users.noreply.github.com>
2024-08-05 03:58:38 +0100

    landing page finalised need optimisation

--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -1,28 +1,38 @@
 import './components.css';
 import Racket from '@/public/racket.svg';
 import { MdOutlineLanguage } from 'react-icons/md';
+import Logo from '@/public/landing-page-logo.svg';
 
 const Card = () => {
   return (
-    <div className="card">
-      <div className="left">
-        <div className="lang-box">
-          <div className="lang">
-            <MdOutlineLanguage />
+    <div className="box">
+      <div className="logo">
+        <Logo />
+      </div>
+      <div className="all">
+        <div className="card">
+          <div className="left">
+            <div className="lang-box">
+              <div className="lang">
+                <MdOutlineLanguage />
+              </div>
+            </div>
+            <div className="title">
+              <h2>Start your own halucinating ping pong journey</h2>
+            </div>
+            <div className="paragraph">
+              <p>
+                welcome to PiPo lorem ipsum ipsum lorem hehe makayn maytgal{' '}
+              </p>
+            </div>
+            <button className="explore">
+              <a href="/home">Explore</a>
+            </button>
+          </div>
+          <div className="right">
+            <Racket />
           </div>
         </div>
-        <div className="title">
-          <h2>Start your own halucinating ping pong journey</h2>
-        </div>
-        <div className="paragraph">
-          <p>welcome to PiPo lorem ipsum ipsum lorem hehe makayn maytgal </p>
-        </div>
-        <button className="explore">
-          <a href="/home">Explore</a>
-        </button>
-      </div>
-      <div className="right">
-        <Racket />
       </div>
     </div>
   );
