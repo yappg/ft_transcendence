0202a50c260e1b48a0dbf02cc7dc61f5b17f9a13
Nourdine <88409152+noureddine-ake@users.noreply.github.com>
2024-08-14 02:46:00 +0100

    fixed push error

--- src/frontend/tailwind.config.ts
+++ src/frontend/tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      theme: {
+        extend: {
+          dropShadow: {
+            custom: '0 -2px 50px rgba(0, 0, 0, 1)',
+          },
+        },
+      },
     },
   },
   plugins: [],
