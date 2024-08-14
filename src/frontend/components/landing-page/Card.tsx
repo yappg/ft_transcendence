ee00ee9120a0171eb93e5ef5d635fcea60a75be8
Nourdine <88409152+noureddine-ake@users.noreply.github.com>
2024-08-14 02:11:59 +0100

    mobile designe done

--- src/frontend/components/landing-page/Card.tsx
+++ src/frontend/components/landing-page/Card.tsx
@@ -3,21 +3,25 @@ import Button from '../Button';
 
 const Card = () => {
   return (
-    <div className="w-full h-full rounded-lg shadow-lg z-[2] flex flex-col justify-center items-center">
-      <div className="w-[45%] h-[18%] mb-[-20%] z-[3]">
+    <div className="w-full h-full rounded-lg shadow-lg z-[2] flex flex-col justify-center items-center md:bg-white">
+      <div className="w-[45%] h-[20vh] mb-[-20%] z-[3]">
         <Logo />
       </div>
       <div className="w-full h-full">
-        <div className="w-full h-full bg-[rgb(88,88,84,0.8)] rounded-tl-[50px] rounded-tr-[50px] flex flex-col">
-          <div className="w-full h-[100%] flex flex-col">
-            <div className="w-full h-[15%] flex">
-              <div className="bg-[rgb(0,0,0,0.5)] h-[55px] w-[55px] rounded-lg absolute top-2 left-2"></div>
+        <div className="w-full h-full bg-[rgb(88,88,84,0.6)] rounded-tl-[40px] rounded-tr-[40px] flex flex-col drop-shadow-custom">
+          <div className="w-full h-[100%] flex flex-col gap-4">
+            <div className="w-full h-[13%] flex">
+              <div className="bg-[rgb(0,0,0,0.5)] h-[6vh] w-[6vh] rounded-lg absolute top-2 left-2"></div>
             </div>
             <div className="w-full h-[35%] px-12">
-              <h1 className='text-[45px]'>Start your own hallucinating ping pong journey</h1>
+              <h1 className="text-[10vw] font-extrabold">
+                Start your own hallucinating ping pong journey
+              </h1>
             </div>
-            <div className="w-full h-[20%] px-12">
-              <p className='text-[25px]'>welcome to PiPo lorem ipsum ipsum lorem hehe makayn maytgal</p>
+            <div className="w-full h-[25%] px-12">
+              <p className="text-[6vw]">
+                welcome to PiPo lorem ipsum ipsum lorem hehe makayn maytgal
+              </p>
             </div>
             <div className="w-full flex-grow flex justify-center items-end py-9 text-[30px]">
               <Button>Explore</Button>
--- src/frontend/postcss.config.mjs
+++ src/frontend/postcss.config.mjs
@@ -1,5 +1,14 @@
+import { draftMode } from 'next/headers';
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
+  theme: {
+    extend: {
+      dropShadow: {
+        custom: '0 -2px 50px rgba(0, 0, 0, 1)',
+      },
+    },
+  },
   plugins: {
     tailwindcss: {},
   },
