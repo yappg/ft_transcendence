c6f49fb73f3052812ece3d219796d86e44a6c36b
Nourdine <88409152+noureddine-ake@users.noreply.github.com>
2024-08-14 02:46:00 +0100

    fixed push error

--- src/frontend/next.config.mjs
+++ src/frontend/next.config.mjs
@@ -4,12 +4,16 @@
 // export default nextConfig;
 
 const nextConfig = {
+  reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
     return config;
+  },
+  devIndicators: {
+    reactRefresh: false,
   },
 };
 
