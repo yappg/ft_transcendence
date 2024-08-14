b91b08a6b03a8325f25c95218c284af6aeb2e197
Nourdine <88409152+noureddine-ake@users.noreply.github.com>
2024-08-14 02:46:00 +0100

    fixed push error

--- src/frontend/postcss.config.mjs
+++ src/frontend/postcss.config.mjs
@@ -1,14 +1,5 @@
-import { draftMode } from 'next/headers';
-
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  theme: {
-    extend: {
-      dropShadow: {
-        custom: '0 -2px 50px rgba(0, 0, 0, 1)',
-      },
-    },
-  },
   plugins: {
     tailwindcss: {},
   },
