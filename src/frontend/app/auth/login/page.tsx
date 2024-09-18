3a9f7faf7f99225233a247d4a4f5265dfef6771f
Noureddine Akebli <nakebli@e3r9p16.1337.ma>
2024-09-18 06:56:03 +0100

    layout done and started on signup form

---
+++ src/frontend/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+
+function Login() {
+  return (
+    <div className="flex size-full flex-col rounded-t-[50px] bg-gradient-radial md:rounded-b-[50px] lg:mr-[-250px] lg:h-auto lg:w-5/6"></div>
+  );
+}
+
+export default Login;
