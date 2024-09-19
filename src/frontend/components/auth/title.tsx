3479ea7ef7f165458b001a325bc03f392ca62d58
Noureddine Akebli <nakebli@e3r8p12.1337.ma>
2024-09-19 05:39:59 +0100

    handled the title component and it's up for both loin and signup pages

--- src/frontend/components/auth/title.tsx
+++ src/frontend/components/auth/title.tsx
@@ -3,17 +3,28 @@ import React from 'react';
 
 function Title() {
   return (
-    <div className="flex h-auto flex-col justify-end">
-      <div className="flex h-1/6 w-full gap-4 pl-4">
-        <img src="/google.svg" alt="google" className="size-[50px]" />
-        <img src="/42.svg" alt="google" className="size-[50px]" />
+    // <div className="flex h-auto w-5/6 flex-col justify-center">
+    //   <div className="mb-[-20px] flex h-[30px] w-full items-center gap-4 pl-4">
+    //     <img src="/google.svg" alt="google" className="h-full" />
+    //     <img src="/42.svg" alt="google" className=" h-full py-1" />
+    //   </div>
+    //   <div className="h-auto w-full font-coustard font-bold">
+    //     <h1 className="h-[50px] text-start text-[50px]  text-white md:h-[80px]">Authenticate</h1>
+    //     <h1 className="">
+    //       <span>your</span> <span className="text-my-red">account</span>
+    //     </h1>
+    //   </div>
+    // </div>
+    <div className="flex h-auto w-3/4 flex-col justify-center lg:w-5/6">
+      <div className="mb-[-15px] flex h-[35px] gap-4 pl-2 md:mb-[-20px] md:h-[40px]">
+        <img src="/google.svg" alt="google" className="h-full" />
+        <img src="/42.svg" alt="google" className=" h-full py-1" />
       </div>
-      <div className="h-auto w-full font-coustard font-bold">
-        <h1 className="h-[50px] text-start text-[50px] text-white md:h-[80px] md:text-[80px]">
-          Authorization
-        </h1>
-        <h1 className="text-[30px] md:text-[45px]">
-          of your <span className="text-my-red">account</span>
+      <div className="h-4/6 font-coustard text-[35px] font-bold">
+        <h1 className="h-[60px] text-[59px] md:h-[80px] md:text-[70px]">Authenticate</h1>
+        <h1>
+          <span>your </span>
+          <span className="text-my-red">account</span>
         </h1>
       </div>
     </div>
