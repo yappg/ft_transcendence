9895213d26a11d9ebfeec5daf3b8225190d1c6bb
Noureddine Akebli <nakebli@e3r8p12.1337.ma>
2024-09-19 06:00:55 +0100

    title done and fixed branch name problem

--- src/frontend/components/auth/title.tsx
+++ src/frontend/components/auth/title.tsx
@@ -3,18 +3,6 @@ import React from 'react';
 
 function Title() {
   return (
-    // <div className="flex h-auto w-5/6 flex-col justify-center">
-    //   <div className="mb-[-20px] flex h-[30px] w-full items-center gap-4 pl-4">
-    //     <img src="/google.svg" alt="google" className="h-full" />
-    //     <img src="/42.svg" alt="google" className=" h-full py-1" />
-    //   </div>
-    //   <div className="h-auto w-full font-coustard font-bold">
-    //     <h1 className="h-[50px] text-start text-[50px]  text-white md:h-[80px]">Authenticate</h1>
-    //     <h1 className="">
-    //       <span>your</span> <span className="text-my-red">account</span>
-    //     </h1>
-    //   </div>
-    // </div>
     <div className="flex h-auto w-3/4 flex-col justify-center lg:w-5/6">
       <div className="mb-[-15px] flex h-[35px] gap-4 pl-2 md:mb-[-20px] md:h-[40px]">
         <img src="/google.svg" alt="google" className="h-full" />
