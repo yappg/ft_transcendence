ca4edc024247589a6e0a062217a381c352674efb
Noureddine Akebli <nakebli@e3r8p12.1337.ma>
2024-09-19 05:39:59 +0100

    handled the title component and it's up for both loin and signup pages

--- src/frontend/app/auth/login/page.tsx
+++ src/frontend/app/auth/login/page.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
+import Title from '@/components/auth/title';
 
 function Login() {
   return (
-    <div className="flex size-full flex-col rounded-t-[50px] bg-gradient-radial md:rounded-b-[50px] lg:mr-[-250px] lg:h-auto lg:w-5/6"></div>
+    <div className="flex size-full flex-col items-center rounded-t-[50px] bg-gradient-radial pt-16 md:rounded-b-[50px] lg:mr-[-250px] lg:h-auto lg:w-5/6 lg:pt-4">
+      <Title />
+    </div>
   );
 }
 
