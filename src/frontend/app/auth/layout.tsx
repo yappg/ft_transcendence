54d19e4266897d7d857bb56706ec953ba1840b02
Noureddine Akebli <nakebli@e1r10p11.1337.ma>
2024-09-22 23:51:19 +0100

    loging page form done

--- src/frontend/app/auth/layout.tsx
+++ src/frontend/app/auth/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex h-screen w-full items-end justify-center overflow-auto bg-[#13191D] md:items-center">
-      <div className="h-[90%] w-full">
+      <div className="flex h-[95%] w-full flex-col">
         <div className="mb-[-75px] flex h-[150px] w-full items-center justify-center md:mb-[-80px] lg:hidden">
           <img src="/landing-page-logo.svg" alt="logo" className="size-[150px] md:size-[200px]" />
         </div>
@@ -34,7 +34,7 @@ export default function RootLayout({
           {/* the max/min with/height of the div under must be defined in the input and text bars of the form so this dive resizes itself based on the children  */}
           <div className="flex size-full min-w-[390px] md:size-[85%] md:max-h-[850px] md:min-h-[500px] md:max-w-[1100px] lg:h-auto">
             {children}
-            <div className="z-[1] order-2 hidden h-full w-[500px] items-center lg:flex">
+            <div className="z-[1] order-2 hidden w-[500px] lg:flex lg:items-center">
               <img src="/logo.svg" alt="logo-shadow.svg" />
             </div>
           </div>
