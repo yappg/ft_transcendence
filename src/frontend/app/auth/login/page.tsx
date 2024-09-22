3d5e9acfabedf70724c11fbaaa0486b331294a2d
Noureddine Akebli <nakebli@e1r10p11.1337.ma>
2024-09-22 23:51:19 +0100

    loging page form done

--- src/frontend/app/auth/login/page.tsx
+++ src/frontend/app/auth/login/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Title from '@/components/auth/title';
-import Form from '@/components/auth/form';
+import { Form, MyLink } from '@/components/auth/form';
 
 function Login() {
   return (
-    <div className="flex size-full flex-col items-start rounded-t-[50px] bg-gradient-radial pl-9 pt-16 sm:pl-16 md:rounded-b-[50px] lg:mr-[-250px] lg:h-auto lg:w-5/6 lg:pr-[250px] lg:pt-4">
+    <div className="costum-shadow flex size-full min-h-[500px] flex-col justify-between rounded-t-[50px] bg-gradient-radial px-9 pt-16 sm:pl-16 md:rounded-b-[50px] lg:mr-[-250px] lg:h-auto lg:w-5/6 lg:pr-[250px] lg:pt-4">
       <Title />
       <Form />
+      <MyLink text="Don't have an account? " href="Sign up" />
     </div>
   );
 }
