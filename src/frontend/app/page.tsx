a81f77e451e40f5a17fc35042c00b8cf063fc06d
Noureddine Akebli <nakebli@e1r10p15.1337.ma>
2024-09-28 14:25:47 +0100

    added shadow

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -2,7 +2,7 @@ import Background from '@/components/landing-page/Background';
 
 export default function Home() {
   return (
-    <div className="relative w-screen h-screen overflow-hidden z-[1] bg-[#13191D]">
+    <div className="relative z-[1] h-screen w-full overflow-hidden bg-[#13191D]">
       <Background />
     </div>
   );
