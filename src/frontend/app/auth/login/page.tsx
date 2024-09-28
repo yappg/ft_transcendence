9b1230308fb2705fccc2f084067482e607c8b037
Noureddine Akebli <nakebli@e1r10p15.1337.ma>
2024-09-28 12:01:12 +0100

    done with login and signup ui need  fied buges existed need to add form functionaliti and button onclick

--- src/frontend/app/auth/login/page.tsx
+++ src/frontend/app/auth/login/page.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import Title from '@/components/auth/title';
 import { FaRegUser } from 'react-icons/fa';
-import { MdOutlineLock } from 'react-icons/md';
+import { RiLock2Line } from 'react-icons/ri';
 import { Form, MyLink } from '@/components/auth/form';
 
 function Login() {
   const fields = [
     { Icon: FaRegUser, placeholder: 'username' },
-    { Icon: MdOutlineLock, placeholder: 'password' },
+    { Icon: RiLock2Line, placeholder: 'password' },
   ];
   return (
-    <div className="costum-shadow flex size-full min-h-[600px] flex-col justify-between rounded-t-[50px] bg-gradient-radial px-9 pt-16 sm:px-16 md:rounded-b-[50px] lg:mr-[-250px] lg:h-auto lg:min-h-[550px] lg:w-5/6 lg:pr-[250px] lg:pt-4">
+    <div className="costum-shadow flex w-full grow flex-col justify-between rounded-t-[50px] bg-gradient-radial px-9 pt-16 sm:px-16 md:rounded-b-[50px] lg:mr-[-250px] lg:h-auto lg:min-h-[550px] lg:w-5/6 lg:pr-[250px] lg:pt-4">
       <Title />
-      <Form fields={fields} />
-      <MyLink text="Have no account yet? " href="Sign up" />
+      <Form fields={fields} buttonlabel="login" />
+      <MyLink text="Have no account yet? " href="signup" />
     </div>
   );
 }
