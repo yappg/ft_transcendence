ebf07f81724031d9baaf9f1ba80945c11c3012dd
Noureddine Akebli <nakebli@e1r10p15.1337.ma>
2024-09-28 12:01:12 +0100

    done with login and signup ui need  fied buges existed need to add form functionaliti and button onclick

--- src/frontend/app/auth/signup/page.tsx
+++ src/frontend/app/auth/signup/page.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import Title from '@/components/auth/title';
 import { Form, MyLink } from '@/components/auth/form';
 import { FaRegUser } from 'react-icons/fa';
-import { MdOutlineLock } from 'react-icons/md';
+import { RiLock2Line } from 'react-icons/ri';
+import { MdOutlineMail } from 'react-icons/md';
 
 function signup() {
   const fields = [
-    { Icon: FaRegUser, placeholder: 'hdfjbsbzdj' },
-    { Icon: FaRegUser, placeholder: 'hdfjbsbzdj' },
+    { Icon: MdOutlineMail, placeholder: 'email' },
     { Icon: FaRegUser, placeholder: 'username' },
-    { Icon: MdOutlineLock, placeholder: 'password' },
+    { Icon: RiLock2Line, placeholder: 'password' },
+    { Icon: RiLock2Line, placeholder: 'verify password' },
   ];
   return (
-    <div className="z-0 order-3 flex size-full flex-col justify-between gap-5 rounded-t-[50px] bg-gradient-radial px-[50px] pt-16 md:rounded-b-[50px] lg:ml-[-250px] lg:h-auto lg:w-5/6 lg:pl-[250px] lg:pt-4">
+    <div className="costum-shadow order-3 flex w-full grow flex-col justify-between gap-3 rounded-t-[50px] bg-gradient-radial px-9 pt-16 sm:px-16 md:rounded-b-[50px] lg:ml-[-250px] lg:min-h-0 lg:w-5/6 lg:pl-[250px] lg:pt-4">
       <Title />
-      <Form fields={fields} />
+      <Form fields={fields} buttonlabel="register" />
       <MyLink text="already have an account ?  " href="login" />
     </div>
   );
