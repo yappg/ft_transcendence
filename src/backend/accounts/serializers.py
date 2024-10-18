26bce6a5c24055e743fb8b924e8ee8fb071b516b
Omar Taraki <otaraki@student.1337.ma>
2024-10-18 03:35:06 +0000

    non sense

--- src/backend/accounts/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Player
-
-class PlayerSerializer(serializers.ModelSerializer):
-    class meta:
-        model=Player
-        field=('id', 'username', 'wins', 'losses',)
-
