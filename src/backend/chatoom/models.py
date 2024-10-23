a3717ab38990fde26217a67488a6a5bb7b2fc192
omar-xy <otaraki@gmail.com>
2024-10-23 23:27:44 +0100

    adjusting the backend to be compatible with <Websocket, ASGI>

--- src/backend/chatoom/models.py
+++ src/backend/chatoom/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
-class ChatRoom(models.Model):
+class chatoom(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='chatrooms')
-    # messages = models.ManyToManyField('ChatMessage', related_name='chatroom')
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='chatooms')
+    # messages = models.ManyToManyField('ChatMessage', related_name='chatoom')
     password = models.CharField(max_length=100, null=True, blank=True)
