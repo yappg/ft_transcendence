a7446b68735b9ded56783e85a06b47e6ea18b705
omar-xy <otaraki@gmail.com>
2024-10-24 13:19:56 +0100

    the chat working for a template example with JS, backend:  updated the consumers.py to work with a chat_group

--- src/backend/chatoom/consumers.py
+++ src/backend/chatoom/consumers.py
@@ -1,15 +1,51 @@
 import json 
 from channels.generic.websocket import WebsocketConsumer
-
+from asgiref.sync import async_to_sync
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        # Join the group
+        async_to_sync(self.channel_layer.group_add)(
+            "chat_group",
+            self.channel_name
+        )
         self.accept()
+        
         self.send(
             text_data=json.dumps({
-                'type': 'cnx_4G machy 2g sir t2wd',
+                'type': 'chat',
                 'message': 'Hello first connection'
             })
         )
-    def respond(self, event):
-        self.send(text_data=json.dumps(event))
+
+    def disconnect(self, close_code):
+        # Leave the group
+        async_to_sync(self.channel_layer.group_discard)(
+            "chat_group",
+            self.channel_name
+        )
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        print('Message:', message)
+        
+        # Broadcast the message to the group
+        async_to_sync(self.channel_layer.group_send)(
+            "chat_group",
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+    
+    # Handler for messages sent to the group
+    def chat_message(self, event):
+        message = event['message']
+        
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'type': 'chat',
+            'message': message
+        }))
+            
