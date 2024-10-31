56e12c7b582b9e5c25ab10cdf800f1c35ebe1fcd
ElmehdiBennix <bennixmehdi@gmail.com>
2024-10-31 02:20:17 +0100

    automatic push

--- src/backend/game/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Game
-
-
-class GameSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Game
-        fields = ('id', 'player1', 'player2', 'player1_score', 'player2_score', 'is_active', 'created_at', 'updated_at')
