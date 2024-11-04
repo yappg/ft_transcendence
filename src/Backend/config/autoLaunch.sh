cf5968d755be04ab2c5259674bb59c0663f5c902
Rambeau <bennixmehdi0@gmail.com>
2024-11-04 11:34:25 +0100

    pulling a working backend branch

--- src/Backend/config/autoLaunch.sh
+++ src/Backend/config/autoLaunch.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-pip install --no-cache-dir -r requirements.txt
+# python manage.py collectstatic --noinput
 
-# python manage.py collectstatic
-
-python manage.py makemigrations
-python manage.py migrate
+# python manage.py makemigrations
+# python manage.py migrate
 
 exec "$@"
