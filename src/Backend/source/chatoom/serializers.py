a2409880ea22b07ee286b458a84b7f8ad1e11860
ElmehdiBennix <bennixmehdi@gmail.com>
2024-11-04 12:56:35 +0100

    fixed depends for django

--- src/Backend/source/chatoom/serializers.py
+++ src/Backend/source/chatoom/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
 from models import ChatRoom
 
-
-Class ChatRoomSerializer(serializers.ModelSerializer):
+class ChatRoomSerializer(serializers.ModelSerializer):
     class meta:
         model = ChatRoom
         fields = ('id', 'name', 'users', 'password')
