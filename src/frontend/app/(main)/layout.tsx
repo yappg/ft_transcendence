e06f6d14f2aa9f645979ce63f86966c13ca5f31a
Meryeme Mesbahi <mmesbahi@e1r8p14.1337.ma>
2024-11-09 20:57:37 +0100

    handle shown icon on every route

--- src/frontend/app/(main)/layout.tsx
+++ src/frontend/app/(main)/layout.tsx
@@ -4,23 +4,39 @@ import '@/app/globals.css';
 import { RightBar } from '@/components/RightBar';
 import { SideBar } from '@/components/SideBar';
 import { Header } from '@/components/header';
-import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  const pathname = usePathname();
   const [isActivated, setIsActivated] = useState(0);
-  const [showSideBarIcon, setShowSideBarIcon] = useState(false);
+  const [showSideBarIcon, setShowSideBarIcon] = useState(true);
+  useEffect(() => {
+    if (pathname === '/friends' || pathname === '/messages') {
+      setShowSideBarIcon(true);
+    } else {
+      setShowSideBarIcon(false);
+    }
+  }, [pathname]);
+
   const handleRightClick = (id: number) => {
     setIsActivated(id);
-    if (id === 7 || id === 6) {
+    if (
+      pathname === '/friends' ||
+      pathname === '/messages' ||
+      id === 6 ||
+      id === 7
+    ) {
       setShowSideBarIcon(true);
     } else {
       setShowSideBarIcon(false);
     }
   };
+
   return (
     <div className="bg-white h-[100vh] w-[100vw] overflow-auto grid p-8 grid-cols-[repeat(11,_1fr)] grid-rows-[repeat(9,_1fr)] gap-[8px]">
       <div className="row-[span_9_/_span_9] flex items-center justify-center min-h-0 flex-grow">
@@ -36,7 +52,7 @@ export default function RootLayout({
         <Header />
       </div>
       <div
-        className={`${isActivated == 7 ? 'hidden' : 'flex'} ${isActivated == 6 ? 'hidden' : 'flex'} row-[span_9_/_span_9] col-start-12 row-start-1 items-center justify-center transition-all duration-300 `}
+        className={`${isActivated === 7 || isActivated === 6 || pathname === '/friends' || pathname === '/messages' ? 'hidden' : 'flex'} row-[span_9_/_span_9] col-start-12 row-start-1 items-center justify-center transition-all duration-300`}
       >
         <RightBar
           handleRightClick={handleRightClick}
