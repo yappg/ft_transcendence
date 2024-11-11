cc9e4d0b718eb66927865b60b8d257f18158d771
omar-xy <otaraki@gmail.com>
2024-11-11 21:28:48 +0100

    merge/working starting on chat

--- src/Backend/source/_1Config/asgi.py
+++ src/Backend/source/_1Config/asgi.py
@@ -8,38 +8,37 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django  # Import django to call django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-
 from django.urls import path
-from django.urls import re_path
-from chat.consumers import ChatConsumer
 from channels.security.websocket import AllowedHostsOriginValidator
-
 from chat.routing import websockets_urlpatterns
+from chat import consumers
 
+# Set the default settings module for the 'django' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_1Config.settings')
 
+# Initialize Django
+django.setup()  # Ensure Django is set up before accessing any models
+
+# Get the ASGI application
 django_asgi_app = get_asgi_application()
+
+# Define the ASGI application routing
 application = ProtocolTypeRouter(
     {
         'http': django_asgi_app,
-        # 'websocket':
-        #     AllowedHostsOriginValidator(
-        #     AuthMiddlewareStack(
-        #         URLRouter(
-        #             websockets_urlpatterns
-        #         )
-        #     )
-        # ),
-        "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter([
-                # path("chat/admin/", AdminChatConsumer.as_asgi()),
-                path('ws/socket-server', ChatConsumer.as_asgi()),
-            ])
-        )
-    ),
-    })
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(
+                    websockets_urlpatterns + [
+                        path('ws/socket-server', consumers.ChatConsumer.as_asgi()),
+                    ]
+                )
+            )
+        ),
+    }
+)
