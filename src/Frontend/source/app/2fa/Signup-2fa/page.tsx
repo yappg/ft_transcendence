3899fa13c097085da0a685cc8545f54680d487ff
Meryeme Mesbahi <mmesbahi@e1r9p15.1337.ma>
2024-11-14 08:14:10 +0100

    add generate and verfiy api in 2fa sign up page

--- src/Frontend/source/app/2fa/Signup-2fa/page.tsx
+++ src/Frontend/source/app/2fa/Signup-2fa/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import Link from 'next/link';
 import React, { useEffect } from 'react';
-import Image from 'next/image';
 import { InputOTPDemo } from '@/components/2fa/InputOTPDemo';
 import { MyButton } from '@/components/generalUi/Button';
 import { useQRCode } from 'next-qrcode';
@@ -10,6 +9,7 @@ import axios from 'axios';
 export default function LoginTFA() {
   const myString = 'Submit';
   const [value, setValue] = React.useState('');
+  const [isValid, setIsValid] = React.useState(true);
   const [QRcode, setQRcode] = React.useState('e');
   const [isLoading, setIsLoading] = React.useState(false);
   useEffect(() => {
@@ -28,12 +28,33 @@ export default function LoginTFA() {
       console.error('Error:', error);
     }
   }, []);
+
+  const sendOtp = async () => {
+    try {
+      const result = await axios.post('http://127.0.0.1:8000/2fa/verifiy-otp/', {
+        username: 'mmesbahi',
+        otp_token: value,
+      });
+      console.log(result);
+      if (result.data.status) {
+        alert('OTP verified');
+      } else {
+        alert('Invalid OTP');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const { Canvas } = useQRCode();
   const handleClick = () => {
-    if (value == '000000') alert('2FA activated successfully');
-    else alert('Invalis OTP');
+    if (value.length !== 6) {
+      setIsValid(false);
+      return;
+    } else {
+      sendOtp();
+    }
   };
-  // if (isLoading) return <h1>Loading...</h1>;
   return (
     <div className="relative flex size-auto flex-col items-center justify-center gap-10 md:h-[90%] md:w-[85%]">
       <h1 className=" font-dayson flex items-center justify-center text-[40px] text-white transition-all duration-300 md:text-[70px]">
@@ -59,12 +80,12 @@ export default function LoginTFA() {
       <div className="mt-20 flex flex-col items-center justify-center gap-[20px] md:mt-0 md:w-full md:gap-[40px]">
         <MyButton
           onClick={handleClick}
-          className="font-coustard h-[68px] w-[201px] rounded-lg text-[24px] 
-      font-black transition-all duration-300 md:h-[88px] md:w-[301px] md:text-[36px]"
+          className="font-dayson h-[68px] w-[201px] rounded-lg text-[24px] font-black
+       transition-all duration-300 md:h-[88px] md:w-[301px] md:text-[36px]"
         >
           {myString}
         </MyButton>
-        <Link href="/Home" className="font-coustard text-[#284F50]">
+        <Link href="/Home" className="font-poppins text-[#284F50]">
           Skip this step
         </Link>
       </div>
