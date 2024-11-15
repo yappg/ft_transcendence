4b15097b5aca5d04a6c05d38696623c43d15610d
Meryeme Mesbahi <mmesbahi@e1r9p14.1337.ma>
2024-11-16 00:56:41 +0100

    add friends page

--- src/Frontend/source/app/(main)/friends/page.tsx
+++ src/Frontend/source/app/(main)/friends/page.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function page() {
-  return <div className="bg-white border-2">friends</div>;
+  return (
+    <div className="bg-side-bar col-span-9 col-start-2 row-span-8 row-start-2 grid grid-cols-5 grid-rows-[repeat(14,_1fr)] rounded-[50px] shadow-2xl">
+      {/* <div className="rounded-[50px]"> */}
+      <div className="col-span-5 row-span-2 border-b border-[#1C1C1C] dark:border-white">1</div>
+      <div className="col-span-5 row-span-2 border-b border-[#1C1C1C] dark:border-white ">2</div>
+      <div className="col-span-5 row-span-2 border-b border-[#1C1C1C] dark:border-white ">3</div>
+      <div className="col-span-5 row-span-2 border-b border-[#1C1C1C] dark:border-white ">4</div>
+      <div className="col-span-5 row-span-2 border-b border-[#1C1C1C] dark:border-white ">5</div>
+      <div className="col-span-5 row-span-2 border-b border-[#1C1C1C] dark:border-white ">6</div>
+      <div className="col-span-5 row-span-2 ">7</div>
+      {/* </div> */}
+    </div>
+  );
 }
 
 export default page;
