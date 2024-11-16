b3b86d2f0f9a7d770e59982595d4ded131f846b8
Meryeme Mesbahi <mmesbahi@e1r13p4.1337.ma>
2024-11-16 02:38:24 +0100

    make all pages synchronized with layout and add friends page

--- src/Frontend/source/app/(main)/friends/page.tsx
+++ src/Frontend/source/app/(main)/friends/page.tsx
@@ -1,19 +1,38 @@
-import React from 'react';
+'use client';
 
-function page() {
+export default function Page() {
   return (
-    <div className="bg-side-bar col-span-9 col-start-2 row-span-8 row-start-2 grid grid-cols-5 grid-rows-[repeat(14,_1fr)] rounded-[50px] shadow-2xl">
-      {/* <div className="rounded-[50px]"> */}
-      <div className="col-span-5 row-span-2 border-b border-[#1C1C1C] dark:border-white">1</div>
-      <div className="col-span-5 row-span-2 border-b border-[#1C1C1C] dark:border-white ">2</div>
-      <div className="col-span-5 row-span-2 border-b border-[#1C1C1C] dark:border-white ">3</div>
-      <div className="col-span-5 row-span-2 border-b border-[#1C1C1C] dark:border-white ">4</div>
-      <div className="col-span-5 row-span-2 border-b border-[#1C1C1C] dark:border-white ">5</div>
-      <div className="col-span-5 row-span-2 border-b border-[#1C1C1C] dark:border-white ">6</div>
-      <div className="col-span-5 row-span-2 ">7</div>
-      {/* </div> */}
+    <div className="col-span-10 col-start-2 row-span-8 row-start-2 grid grid-cols-[1fr] grid-rows-[1fr] pl-6">
+      <div className=" bg-side-bar h-full w-[95%] overflow-hidden rounded-[50px] bg-red-400 shadow-2xl">
+        <div className="h-[150px] w-full bg-green-500"></div>
+        <div className="custom-scrollbar-container h-[calc(100%-200px)] overflow-y-scroll">
+          <div className="h-[150px] w-full bg-red-300"></div>
+          <div className="h-[150px] w-full bg-blue-500"></div>
+          <div className="h-[150px] w-full bg-yellow-500"></div>
+          <div className="h-[150px] w-full bg-red-500"></div>
+          <div className="h-[150px] w-full bg-green-500"></div>
+          <div className="h-[150px] w-full bg-blue-500"></div>
+          <div className="h-[150px] w-full bg-yellow-500"></div>
+          <div className="h-[150px] w-full bg-red-500"></div>
+          <div className="h-[150px] w-full bg-green-500"></div>
+          <div className="h-[150px] w-full bg-blue-500"></div>
+          <div className="h-[150px] w-full bg-yellow-500"></div>
+          <div className="h-[150px] w-full bg-red-500"></div>
+          <div className="h-[150px] w-full bg-green-500"></div>
+          <div className="h-[150px] w-full bg-blue-500"></div>
+          <div className="h-[150px] w-full bg-yellow-500"></div>
+          <div className="h-[150px] w-full bg-red-500"></div>
+          <div className="h-[150px] w-full bg-green-500"></div>
+          <div className="h-[150px] w-full bg-blue-500"></div>
+          <div className="h-[150px] w-full bg-yellow-500"></div>
+          <div className="h-[150px] w-full bg-red-500"></div>
+          <div className="h-[150px] w-full bg-green-500"></div>
+          <div className="h-[150px] w-full bg-blue-500"></div>
+          <div className="h-[150px] w-full bg-yellow-500"></div>
+          <div className="h-[150px] w-full bg-red-500"></div>
+          <div className="h-[150px] w-full bg-green-500"></div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default page;
