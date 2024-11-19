7cad12f4a8eedcf20d313b5f8a2fe302e17cd08e
Meryeme Mesbahi <mmesbahi@e1r8p14.1337.ma>
2024-11-19 19:09:56 +0100

    update the main layout of the pages

--- src/Frontend/source/components/friends/FriendsComponent.tsx
+++ src/Frontend/source/components/friends/FriendsComponent.tsx
@@ -1,5 +1,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { FaCommentDots } from 'react-icons/fa';
+import Link from 'next/link';
 const FriendsComponent = ({
   key,
   name,
@@ -28,33 +29,42 @@ const FriendsComponent = ({
     },
   ];
   return (
-    <div className="bg-side-bar flex h-[150px] w-full flex-row items-center gap-[500px] border-b-2 border-[#1C1C1C] border-opacity-[40%] px-14">
-      <div className="flex h-[75px] w-auto flex-row items-center justify-center gap-10">
-        <Avatar className="size-[75px]">
+    <div className="bg-side-bar flex h-[150px] w-full flex-row items-center justify-between border-b-2 border-[#1C1C1C] border-opacity-[40%] px-14">
+      <div className="flex h-[75px] w-fit flex-row items-center justify-center gap-10">
+        <Avatar className="size-[65px] transition-all duration-300 md:size-[70px] lg:size-[75px]">
           <AvatarImage src={ProfilePhoto} />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
-        <div className="flex h-[75px] flex-col">
-          <h1 className="font-coustard text-[28px] font-black text-[#1C1C1C] opacity-[90%] dark:text-white">
+        <div className="flex h-[75px] w-fit flex-col ">
+          <h1 className="font-coustard text-[20px] font-black text-[#1C1C1C] opacity-[90%] transition-all duration-300 lg:text-[25px] xl:text-[30px] dark:text-white">
             {name}
           </h1>
-          <h1 className="font-coustard text-[25px] text-white opacity-[30%]">level {level}</h1>
+          <h1 className="font-coustard text-[15px] text-white opacity-[30%] transition-all duration-300 md:text-[20px] xl:text-[25px]">
+            level {level}
+          </h1>
         </div>
       </div>
-      <div className="relative flex size-auto flex-row">
+      <div className="relative flex size-auto w-fit flex-row ">
         {achievements.map((achievement, index) => (
-          <Avatar key={index} className="-ml-[17px] size-[75px] ">
+          <Avatar
+            key={index}
+            className="-ml-[17px] size-[60px] transition-all duration-300 xl:size-[75px]"
+          >
             <AvatarImage src={achievement.icon} />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
         ))}
       </div>
-      <div className="">
-        <h1 className="font-coustard font-regular dark:text[#E43222] text-[30px] text-[#28AFB0]">
+      <div className="w-fit ">
+        <h1 className="font-coustard text-[#28AFB0] transition-all duration-300 md:text-[23px] lg:text-[26px] xl:text-[30px] dark:text-[#E43222]">
           Wins {wins}/70
         </h1>
       </div>
-      <FaCommentDots className="text-[#1C1C1C] size-[300px]" />
+      <div className="flex items-center justify-center">
+        <Link href="/messages">
+          <FaCommentDots className="size-[48px] text-[#1C1C1C] opacity-40 transition-all duration-300 xl:size-[55px] dark:text-[#B8B8B8]" />
+        </Link>
+      </div>
     </div>
   );
 };
--- src/Frontend/source/components/friends/UserFriendsNav.tsx
+++ src/Frontend/source/components/friends/UserFriendsNav.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-const UserFriendsNav = ({ player }: { player: player }): JSX.Element => {
+import { friends } from '@/constants/friendsList';
+import FriendsComponent from '@/components/friends/FriendsComponent';
+const UserFriendsNav = ({ player }: { player: typeof player }): JSX.Element => {
   const { name, level } = player;
-  const [activeIndex, setActiveIndex] = useState<number | null>(null); // State to track the active header
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const handleClick = (index: number) => {
     setActiveIndex(index);
@@ -14,34 +16,70 @@ const UserFriendsNav = ({ player }: { player: player }): JSX.Element => {
     { title: 'Invitations', href: '' },
     { title: 'Add New', href: '' },
   ];
+  const renderContent = () => {
+    if (activeIndex === 0) {
+      return (
+        <div className="custom-scrollbar-container h-[calc(100%-200px)] overflow-y-scroll">
+          {friends.map((friend, index) => (
+            <FriendsComponent
+              key={index}
+              name={friend.name}
+              ProfilePhoto={friend.profilePhoto}
+              level={friend.level}
+              wins={friend.wins}
+            />
+          ))}
+        </div>
+      );
+    } else if (activeIndex === 1) {
+      return (
+        <div className="flex size-full items-center justify-center">
+          <h1 className="text-[30px] text-white">No Invitations</h1>
+        </div>
+      );
+    } else if (activeIndex === 2) {
+      return (
+        <div className="flex size-full items-center justify-center">
+          <h1 className="text-[30px] text-white">Add New</h1>
+        </div>
+      );
+    }
+  };
   return (
-    <div className="flex flex-row items-center gap-[800px]">
-      <div className="flex flex-row gap-4">
-        <Avatar className="size-[150px]">
-          <AvatarImage src="/ProfilePhoto.svg" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-        <div className="flex flex-col p-6">
-          <h1 className="font-dayson text-[35px] text-white opacity-[80%]">{name}</h1>
-          <h1 className="font-coustard text-[30px] text-white opacity-[40%]">Level {level}</h1>
+    <div className="flex size-full w-full flex-col items-start justify-start">
+      <div className="friend-bar-bg flex h-[150px] w-full flex-row items-center justify-between px-9">
+        <div className="flex flex-row gap-4">
+          <Avatar className="md:size-[100px] 2xl:size-[150px]">
+            <AvatarImage src="/ProfilePhoto.svg" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col p-6">
+            <h1 className="font-dayson text-[20px] text-white opacity-[80%] md:text-[27px] lg:text-[25px] 2xl:text-[35px]">
+              {name}
+            </h1>
+            <h1 className="font-coustard text-white opacity-[40%] md:text-[22px] lg:text-[26px] 2xl:text-[30px]">
+              Level {level}
+            </h1>
+          </div>
+        </div>
+        <div className="flex h-[60px] w-auto flex-row transition-all duration-300 md:gap-[60px] lg:gap-[100px] xl:gap-[125px] 2xl:gap-[180px]">
+          {headers.map((header, index) => (
+            <Link href={header.href} key={index}>
+              <h1
+                className={`${
+                  activeIndex === index
+                    ? 'border-b-2 border-[#28AFB0] text-[#28AFB0] opacity-[100%] dark:border-[#E43222] dark:text-[#E43222]'
+                    : 'text-white opacity-[60%]'
+                } font-dayson cursor-pointer transition-all duration-300 md:text-[20px] lg:text-[18px] xl:text-[25px] 2xl:text-[30px]`}
+                onClick={() => handleClick(index)}
+              >
+                {header.title}
+              </h1>
+            </Link>
+          ))}
         </div>
       </div>
-      <div className="flex h-[60px] w-auto flex-row gap-20">
-        {headers.map((header, index) => (
-          <Link href={header.href} key={index}>
-            <h1
-              className={`${
-                activeIndex === index
-                  ? 'border-b-2 border-[#28AFB0] text-[#28AFB0] opacity-[100%]'
-                  : 'text-white opacity-[60%]'
-              } font-dayson cursor-pointer text-[30px] transition-all duration-300`}
-              onClick={() => handleClick(index)}
-            >
-              {header.title}
-            </h1>
-          </Link>
-        ))}
-      </div>
+      <div className="h-[90%] w-full ">{renderContent()}</div>
     </div>
   );
 };
