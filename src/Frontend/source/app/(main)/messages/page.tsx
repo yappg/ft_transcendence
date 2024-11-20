6b00209b3a29001087551abb780324e0749dbb8b
Noureddine Akebli <nakebli@e1r10p15.1337.ma>
2024-11-20 04:11:41 +0100

    added annimation to authentication

---
+++ src/Frontend/source/app/(main)/messages/page.tsx
@@ -0,0 +1,77 @@
+/* eslint-disable tailwindcss/no-custom-classname */
+'use client';
+import InputBar from '@/components/auth/input-bar';
+import React, { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { z } from 'zod';
+import FriendsChat from '@/components/chat/friendsChat';
+import { FChatProps } from '@/constants/chat';
+import { FiPlus } from 'react-icons/fi';
+
+type FieldType = 'input' | 'password' | 'email' | 'text' | 'number' | 'date';
+
+interface FormField {
+  Icon: React.ElementType;
+  placeholder: string;
+  value: string;
+  type: FieldType;
+  setValue: (value: string) => void;
+  validation?: z.ZodType<any>;
+}
+const FormMaker = () => {
+  const [value, setValue] = useState('');
+  return {
+    Icon: FaSearch,
+    placeholder: 'make a search',
+    value: '',
+    type: 'password' as const,
+    setValue: () => {},
+    validation: z.string().min(8, 'Password must be at least 8 characters'),
+  };
+};
+
+const app = () => {
+  const field = FormMaker();
+  return (
+    <div className="bg-linear-gradient dark:bg-linear-gradient-dark flex h-screen w-full px-11 py-8">
+      <div className="grid size-full grid-cols-3 gap-6 overflow-hidden p-3">
+        <div className="col-start-1 col-end-3 hidden items-end justify-center rounded-2xl bg-slate-100 p-4 lg:flex">
+          <div className="bg-secondary flex h-[60px]  w-11/12 items-center gap-4 rounded-md px-4">
+            <div className="flex size-full items-center  gap-3 px-4">
+              <FiPlus className="size-[30px]" />
+              <input
+                placeholder="Start a new conversation"
+                className="text-primary bg-transparent"
+              />
+            </div>
+            <div className="flex size-[40px] items-center justify-center bg-slate-500">{'>>'}</div>
+          </div>
+        </div>
+        <div className="bg-black-crd costum-little-shadow relative col-start-1 col-end-4 overflow-hidden rounded-2xl lg:col-start-3">
+          <div className="bg-black-crd sticky top-0 flex h-[120px] w-full items-center justify-center gap-4 px-8">
+            <InputBar
+              className="w-auto rounded-[10px] px-4 py-2"
+              Icon={field.Icon}
+              placeholder={field.placeholder}
+              value={field.value}
+              type={field.type}
+              setValue={field.setValue}
+              error={'hello'}
+              onBlur={() => {}}
+            />
+            <div className="bg-white-crd flex size-fit items-center justify-center rounded-[10px] border border-[rgb(0,0,0,0.8)] p-[5px] dark:border-white">
+              <FiPlus className="size-[30px] text-[rgb(0,0,0,0.8)] dark:text-white" />
+            </div>
+          </div>
+          <div className="no-scrollbar h-full overflow-y-scroll">
+            {FChatProps.map((data, index) => (
+              <FriendsChat key={index} {...data} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default app;
--- src/Frontend/source/app/auth/layout.tsx
+++ src/Frontend/source/app/auth/layout.tsx
@@ -1,12 +1,32 @@
+/* eslint-disable tailwindcss/enforces-negative-arbitrary-values */
 /* eslint-disable tailwindcss/no-custom-classname */
 /* eslint-disable @next/next/no-img-element */
+'use client';
 import Link from 'next/link';
-
+import { useState, useEffect } from 'react';
+import { usePathname } from 'next/navigation';
+import Card from '@/components/generalUi/Card';
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isLeft, setIsLeft] = useState(true);
+  const pathname = usePathname();
+  const [isAnimating, setIsAnimating] = useState(false); // Controls animation state
+
+  useEffect(() => {
+    // Trigger animation
+    setIsAnimating(true);
+
+    // Wait for animation to complete before updating position
+    const timer = setTimeout(() => {
+      setIsLeft(pathname === '/auth/signup');
+      setIsAnimating(false);
+    }, 10); // Match the animation duration (2s)
+
+    return () => clearTimeout(timer); // Cleanup timeout on unmount
+  }, [pathname]);
   return (
     <div className="bg-linear-gradient dark:bg-linear-gradient-dark flex h-screen w-full items-end justify-start overflow-auto">
       <div className="z-0 flex h-[95%] w-full min-w-[350px] flex-col items-start">
@@ -24,12 +44,30 @@ export default function RootLayout({
             />
           </Link>
         </div>
-        <div className="flex w-full grow items-start justify-start md:justify-center lg:items-start lg:py-[12vh]">
-          <div className="flex size-full justify-start md:size-[85%] md:h-auto md:max-h-[850px] md:min-h-[500px] md:max-w-[1100px]">
+        <div className="flex w-full grow items-start justify-center md:justify-center lg:items-start lg:py-[12vh]">
+          {/* <div className="flex size-full justify-start md:size-[85%] md:h-auto md:max-h-[850px] md:min-h-[500px] md:max-w-[1100px]">
             {children}
             <Link href="/" className="z-[1] order-2 hidden w-[550px] lg:flex lg:items-center">
               <img src="/logo.svg" alt="logo-shadow.svg" />
             </Link>
+          </div> */}
+          <div
+            className={`relative flex size-fit items-center transition-all duration-1000 ease-in-out ${
+              !isLeft ? 'lg:mr-[120px]' : 'lg:ml-[120px]'
+            }`}
+          >
+            <Card
+              className={`px-9 pt-16 sm:px-16 md:size-[85%] md:h-auto md:max-h-[850px]  md:min-h-[500px] md:w-[600px] md:rounded-b-[30px] lg:w-[850px] lg:max-w-[850px]  ${isLeft ? 'lg:pl-[230px]' : 'lg:pr-[230px]'} lg:pt-4`}
+            >
+              {children}
+            </Card>
+            <div
+              className={`absolute  transition-all duration-500 ease-in-out ${
+                !isLeft ? 'ml-[570px]' : 'ml-[-200px]'
+              } z-[1] hidden w-[460px] lg:flex lg:items-center`}
+            >
+              <img src="/logo.svg" alt="logo-shadow.svg" />
+            </div>
           </div>
         </div>
       </div>
