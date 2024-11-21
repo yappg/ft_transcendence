67337a7c837b1392559da829e1829edbbe49b6d0
mmes-bahi <mmesbahi@student.1337.ma>
2024-11-21 16:34:03 +0100

    fix: resolved SideBarProvider warning and updated RootLayout logic

--- src/Frontend/source/app/(main)/friends/page.tsx
+++ src/Frontend/source/app/(main)/friends/page.tsx
@@ -2,10 +2,12 @@
 import UserFriendsNav from '@/components/friends/UserFriendsNav';
 import { SideBarContext } from '@/context/SideBarContext';
 import { useContext } from 'react';
-
+import { useEffect } from 'react';
 export default function Page() {
   const { setIsActivated } = useContext(SideBarContext);
-  setIsActivated(6);
+  useEffect(() => {
+    setIsActivated(6);
+  }, []);
   const player = {
     name: 'Noureddine Akebli',
     level: 22,
@@ -13,7 +15,7 @@ export default function Page() {
   return (
     <div className="col-span-10 col-start-2 row-span-8 row-start-2 grid grid-cols-[1fr] grid-rows-[1fr] pl-6">
       <div className="h-full w-[95%] overflow-hidden rounded-[50px] shadow-2xl md:w-full">
-        <UserFriendsNav player={player} />
+        <UserFriendsNav/>
       </div>
     </div>
   );
