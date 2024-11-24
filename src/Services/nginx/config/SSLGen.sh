85fc328f5d2380501a780b43f01f3d3ad48f0257
ElmehdiBennix <bennixmehdi@gmail.com>
2024-11-24 04:27:35 +0100

    grafana/prom done

--- src/Services/nginx/config/SSLGen.sh
+++ src/Services/nginx/config/SSLGen.sh
@@ -3,6 +3,6 @@
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout /etc/ssl/private/nginx.key \
     -out /etc/ssl/certs/nginx.crt \
-    -subj "/C=MA/ST=beni_mellal/L=khouribga/O=1337/OU=1337/CN=ebennix.42.fr"
+    -subj "/C=MA/ST=beni_mellal/L=khouribga/O=1337/OU=1337/CN=trans.42.fr"
 
 exec "$@"
