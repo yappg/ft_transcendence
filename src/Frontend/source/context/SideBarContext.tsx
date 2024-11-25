2fd8b2b064aafd2e52ce77752e7a7bd26d125377
Meryeme Mesbahi <mmesbahi@e1r8p14.1337.ma>
2024-11-25 19:37:12 +0100

    fix: persist active tab state across page reloads using localStorage

--- src/Frontend/source/context/SideBarContext.tsx
+++ src/Frontend/source/context/SideBarContext.tsx
@@ -4,23 +4,18 @@ import React, { createContext, useState } from 'react';
 interface SideBarContextProps {
   isActivated: number;
   setIsActivated: React.Dispatch<React.SetStateAction<number>>;
-  activeIndex:number;
-  setActiveIndex: React.Dispatch<React.SetStateAction<number>>;
 }
 
 export const SideBarContext = createContext<SideBarContextProps>({
   isActivated: 0,
   setIsActivated: () => {},
-  activeIndex: 0,
-  setActiveIndex: () => {},
 });
 
 export const SideBarProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [isActivated, setIsActivated] = useState<number>(1);
-  const [activeIndex, setActiveIndex] = useState<number>(0);
 
   return (
-    <SideBarContext.Provider value={{ isActivated, setIsActivated, activeIndex, setActiveIndex }}>
+    <SideBarContext.Provider value={{ isActivated, setIsActivated }}>
       {children}
     </SideBarContext.Provider>
   );
