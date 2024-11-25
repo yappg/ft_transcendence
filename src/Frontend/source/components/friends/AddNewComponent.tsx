ef726c2a109dec07bac5a72822d3fde3f51b89a7
Meryeme Mesbahi <mmesbahi@e1r4p3.1337.ma>
2024-11-25 21:50:54 +0100

    start working on addNew bar

---
+++ src/Frontend/source/components/friends/AddNewComponent.tsx
@@ -0,0 +1,8 @@
+const AddNew = () => {
+  return (
+    <div className="bg-black-crd flex size-full items-start justify-center p-10">
+      <div className="h-[100px] w-[95%] rounded-[50px] bg-white "></div>
+    </div>
+  );
+};
+export default AddNew;
