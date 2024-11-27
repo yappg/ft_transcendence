72745d05ec37156fab344a5180c5ce47328442e5
Meryeme Mesbahi <mmesbahi@e1r3p11.1337.ma>
2024-11-27 01:24:13 +0100

    FEAT:freins page:added invitations and add new friend sections

--- src/Frontend/source/components/friends/AddNewComponent.tsx
+++
@@ -1,8 +0,0 @@
-const AddNew = () => {
-  return (
-    <div className="bg-black-crd flex size-full items-start justify-center p-10">
-      <div className="h-[100px] w-[95%] rounded-[50px] bg-white "></div>
-    </div>
-  );
-};
-export default AddNew;
