d25135d6db556ee178f8d406dd4700fd7049f003
omar-xy <otaraki@gmail.com>
2024-11-28 05:00:41 +0100

    commit

--- src/Frontend/source/components/friends/AddFriendsComponent.tsx
+++ src/Frontend/source/components/friends/AddFriendsComponent.tsx
@@ -3,11 +3,42 @@ import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import FriendRequestCard from './FriendRequestCard';
 import { IconPlus } from '@tabler/icons-react';
+
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+
 const AddFriends = () => {
   const [searchQuery, setsearchQuery] = useState('');
+  const [message, setMessage] = useState('');
   const FiltredUsers = UsersList.filter((User) =>
     User.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
+
+
+
+  const sendFriendRequest = async (rev_id: BigInteger ,receiverUsername: string) => {
+    try {
+      const token = Cookies.get('access_token'); // Retrieve the access token from cookies
+      const response = await axios.post('http://localhost:8080/relations/friends/invite', {
+        id: rev_id,
+        receiver: receiverUsername
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}` // Use the token from cookies
+        }
+      });
+
+      if (response.status === 201) {
+        setMessage(`Friend request sent to ${receiverUsername}`);
+      } else {
+        setMessage(`Error sending friend request to ${receiverUsername}`);
+      }
+    } catch (error) {
+      setMessage(`Error sending friend request to ${receiverUsername}: ${error.response ? error.response.data : error.message}`);
+    }
+  };
   return (
     <div className="bg-black-crd flex size-full flex-col items-center justify-between gap-10 overflow-visible rounded-lg pt-10">
       <Form className="flex h-[70px] w-[65%] items-center justify-center rounded-[30px] bg-cyan-100 bg-opacity-20 shadow-2xl">
@@ -28,12 +59,19 @@ const AddFriends = () => {
               name={user.name}
               ProfilePhoto={user.ProfilePhoto}
               vari={user.level}
-              actions={[<IconPlus key={user.id} className="ml-[100px] size-[40px] text-white" />]}
+              actions={[
+                <IconPlus
+                  key={user.id}
+                  className="ml-[100px] size-[40px] text-white cursor-pointer"
+                  onClick={() => sendFriendRequest(user.id, user.name)}
+                />
+              ]}
               customStyles={{ backgroundColor: 'transparent' }}
             />
           ))
         )}
       </div>
+      {message && <div className="text-center font-bold text-white">{message}</div>}
     </div>
   );
 };
