4ce7575f817b1ac3ac72f3697a9a7c08c1bbdfca
omar-xy <otaraki@gmail.com>
2024-12-02 18:43:06 +0100

    ...

--- src/Frontend/source/components/chat/Messages.tsx
+++ src/Frontend/source/components/chat/Messages.tsx
@@ -6,7 +6,7 @@ import { IoSend } from 'react-icons/io5';
 import { FiPlus } from 'react-icons/fi';
 import Cookies from 'js-cookie';
 import { chatService  } from '@/services/chatService';
-import { Message, User, Chat } from '@/constants/chat';
+import { Message, User } from '@/constants/chat';
 
 interface MessagesProps {
   chatId: number;
@@ -147,68 +147,68 @@ export const Messages: React.FC<MessagesProps> = ({ chatId, chatPartner, message
   );
 };
 
-interface ChatItemProps {
-  chat: Chat;
-  onClick: () => void;
-  users: { [key: number]: User };
-  selectedChat: Chat | null;
-}
+// interface ChatItemProps {
+//   chat: Chat;
+//   onClick: () => void;
+//   users: { [key: number]: User };
+//   selectedChat: Chat | null;
+// }
 
-const ChatItem: React.FC<ChatItemProps> = ({ chat, onClick, users, selectedChat }) => {
-  const { senders, last_message } = chat;
-  const participant = senders[0]; // Assuming the first participant is the one to display
-  const user = users[participant.id];
+// const ChatItem: React.FC<ChatItemProps> = ({ chat, onClick, users, selectedChat }) => {
+//   const { senders, last_message } = chat;
+//   const participant = senders[0]; // Assuming the first participant is the one to display
+//   const user = users[participant.id];
 
-  return (
-    <div onClick={onClick} className={`flex h-[100px] w-full items-center justify-between gap-2 border-b border-gray-400 px-2 text-white cursor-pointer ${selectedChat && selectedChat.id === chat.id ? 'bg-gray-700' : ''}`}>
-      <div className="flex h-full w-3/4 items-center justify-start gap-2 p-2">
-        <div className="flex size-[60px] items-center justify-center rounded-full">
-          {user?.profile_picture ? (
-            <img src={user.profile_picture} alt={`${user.username} PDP`} />
-          ) : (
-            <div className="bg-gray-500 rounded-full w-full h-full flex items-center justify-center">
-              <span>{user?.username.charAt(0)}</span>
-            </div>
-          )}
-        </div>
-        <div className="w-4/5">
-          <h1>{user?.username}</h1>
-          <p className="line-clamp-1 w-full text-ellipsis text-[rgb(255,255,255,0.5)]">{last_message?.content || 'No messages yet'}</p>
-        </div>
-      </div>
-      <div className="flex flex-col items-center gap-2">
-        <h3 className="text-sm text-[rgb(255,255,255,0.5)]">
-          {last_message ? new Date(last_message.timestamp).toLocaleTimeString() : ''}
-        </h3>
-        <div
-          className={`${0 > 0 ? 'dark:bg-primary-dark bg-green-300' : 'border border-[rgb(255,255,255,0.5)] bg-transparent text-[rgb(255,255,255,0.5)]'} flex size-[20px] items-center justify-center rounded-full text-xs`}
-        >
-          {0 > 0 ? 0 : <RiCheckDoubleLine />}
-        </div>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div onClick={onClick} className={`flex h-[100px] w-full items-center justify-between gap-2 border-b border-gray-400 px-2 text-white cursor-pointer ${selectedChat && selectedChat.id === chat.id ? 'bg-gray-700' : ''}`}>
+//       <div className="flex h-full w-3/4 items-center justify-start gap-2 p-2">
+//         <div className="flex size-[60px] items-center justify-center rounded-full">
+//           {user?.profile_picture ? (
+//             <img src={user.profile_picture} alt={`${user.username} PDP`} />
+//           ) : (
+//             <div className="bg-gray-500 rounded-full w-full h-full flex items-center justify-center">
+//               <span>{user?.username.charAt(0)}</span>
+//             </div>
+//           )}
+//         </div>
+//         <div className="w-4/5">
+//           <h1>{user?.username}</h1>
+//           <p className="line-clamp-1 w-full text-ellipsis text-[rgb(255,255,255,0.5)]">{last_message?.content || 'No messages yet'}</p>
+//         </div>
+//       </div>
+//       <div className="flex flex-col items-center gap-2">
+//         <h3 className="text-sm text-[rgb(255,255,255,0.5)]">
+//           {last_message ? new Date(last_message.timestamp).toLocaleTimeString() : ''}
+//         </h3>
+//         <div
+//           className={`${0 > 0 ? 'dark:bg-primary-dark bg-green-300' : 'border border-[rgb(255,255,255,0.5)] bg-transparent text-[rgb(255,255,255,0.5)]'} flex size-[20px] items-center justify-center rounded-full text-xs`}
+//         >
+//           {0 > 0 ? 0 : <RiCheckDoubleLine />}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-interface ChatListProps {
-  onChatSelect: (chat: Chat) => void;
-  selectedChat: Chat | null;
-  chats: Chat[];
-  users: { [key: number]: User };
-}
+// interface ChatListProps {
+//   onChatSelect: (chat: Chat) => void;
+//   selectedChat: Chat | null;
+//   chats: Chat[];
+//   users: { [key: number]: User };
+// }
 
-export const ChatList: React.FC<ChatListProps> = ({ onChatSelect, selectedChat, chats, users }) => {
-  return (
-    <div className="chat-list">
-      {chats.map(chat => (
-        <ChatItem
-          key={chat.id}
-          chat={chat}
-          onClick={() => onChatSelect(chat)}
-          users={users}
-          selectedChat={selectedChat}
-        />
-      ))}
-    </div>
-  );
-};
+// export const ChatList: React.FC<ChatListProps> = ({ onChatSelect, selectedChat, chats, users }) => {
+//   return (
+//     <div className="chat-list">
+//       {chats.map(chat => (
+//         <ChatItem
+//           key={chat.id}
+//           chat={chat}
+//           onClick={() => onChatSelect(chat)}
+//           users={users}
+//           selectedChat={selectedChat}
+//         />
+//       ))}
+//     </div>
+//   );
+// };
