ea32e85aa6935a5091ee6a3054a4b0a4dafd2b2e
Nourdine <akeblinourdine14@gmail.com>
2024-12-03 22:56:57 +0100

    auth frontend done

---
+++ src/Frontend/source/hooks/fetch-auth.ts
@@ -0,0 +1,31 @@
+export class AuthClient {
+    private static readonly BASE_URL = 'http://localhost:8080/api/auth';
+    private static async fetchWithAuth(endpoint: string, data: Record<string, any>) {
+      delete data.password2
+      try {
+        const response = await fetch(`${AuthClient.BASE_URL}/${endpoint}/`, {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        });
+        return await response.json();
+      } catch (error) {
+        if (error instanceof Error) {
+          throw new Error(`Authentication error: ${error.message}`);
+        }
+  
+        throw new Error('Authentication failed');
+      }
+    }
+  
+    static async signup(data: Record<string, any>) {
+      return this.fetchWithAuth('signup', data);
+    }
+  
+    static async signin(data: Record<string, any>) {
+      return this.fetchWithAuth('signin', data);
+    }
+  }
