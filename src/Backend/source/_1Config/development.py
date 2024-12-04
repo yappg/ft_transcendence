39a6a47cf0fe6d9d51ae82dff7da9f5472458768
omar-xy <otaraki@gmail.com>
2024-12-04 02:37:37 +0100

    adding notifications system still have bugs

--- src/Backend/source/_1Config/development.py
+++ src/Backend/source/_1Config/development.py
@@ -95,7 +95,7 @@ REST_FRAMEWORK = {
     'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES':[
-        'accounts.authenticate.CotumAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_THROTTLE_RATES' : {
         'anon' : '3/min',
@@ -136,22 +136,15 @@ CHANNEL_LAYERS = {
     },
 }
 
+
+
 WSGI_APPLICATION = '_1Config.wsgi.application'
 ASGI_APPLICATION = '_1Config.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv("POSTGRES_DB"),
-#         'USER': os.getenv("POSTGRES_USER"),
-#         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-#         'HOST': 'database',
-#         'PORT': '5432',
-#     }
-# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
