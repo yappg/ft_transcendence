d8f6b4dc40325b2caef56ef8dc95a7f7250c6f5c
ElmehdiBennix <bennixmehdi@gmail.com>
2024-12-04 02:02:16 +0100

    finished accounts rest and user endpoints

--- src/Backend/source/accounts/serializers.py
+++ src/Backend/source/accounts/serializers.py
@@ -28,12 +28,6 @@ from django.contrib.auth import authenticate
     #     "verified_otp": false,
     # }
 
-class MatchHistorySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = MatchHistory
-        fields = '__all__'
-        read_only_fields = ['__all__']
 
 class PlayerSettingsSerializer(serializers.ModelSerializer):
     class Meta:
@@ -43,7 +37,7 @@ class PlayerSettingsSerializer(serializers.ModelSerializer):
 
 
 class PlayerProfileSerializer(serializers.ModelSerializer): # fix private profile only give back display name
-
+### make sure that the display name field is always unic un every put and patch method return error display_name exists
     class Meta:
         model=PlayerProfile
         exclude = ['player']
@@ -51,46 +45,27 @@ class PlayerProfileSerializer(serializers.ModelSerializer): # fix private profil
         # backend_emplemt= ['TT', 'FFFFFFFFF','FFFFFFFFFFF' ,'FFFFFFFFFFFF' ,'FFFFFFFFF' ,'FFFFFFFFFF' ,'TTTTTTTTT' ,'FFFFFFFFFF' ,'TTTTTTTTTT']
 
 ## add a api endpoint to trigger password reset and username change or email > posibly trigger 2fa or email verif
+
+class MatchHistorySerializer(serializers.ModelSerializer):
+    player = PlayerProfileSerializer(read_only=True)
+    opponent = PlayerProfileSerializer(read_only=True)  # Corrected field name
+
+    class Meta:
+        model = MatchHistory
+        fields = '__all__'
+
 class PlayerSerializer(serializers.ModelSerializer): # KEEP IS_ACTIVE AND IS_STAFF LOGIC FOR BACKEND
     profile = PlayerProfileSerializer(read_only=True)
     settings = PlayerSettingsSerializer(read_only=True)
 
     class Meta:
         model = Player
-        # fields = '__all__'
-        fields = ['profile', 'enabled_2fa', 'otp_secret_key', 'verified_otp', 'date_joined']
+        fields = '__all__'
+        # fields = ['profile', '']
         read_only_fields = ['date_joined']
 
-############################################
 
-class SignInSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
-    otp_token = serializers.CharField(max_length=6, required=False)
-    extra_kwargs = {
-        'password':{'write_only':True}
-    }
-
-    def validate(self, attrs):
-        usernm = attrs.get('username')
-        passwd = attrs.get('password')
-        if usernm and passwd:
-            user = authenticate(username=usernm, password=passwd)
-            if not user:
-                raise serializers.ValidationError("Invalid Credentials")
-            # if the the otp would be sent with the user credentials
-            # if user.enabled_2fa:
-            #     if attrs.get('otp_token'):
-            #         totp = pyotp.TOTP(user.otp_secret_key)
-            #         if not totp.verify(otp_token):
-            #             raise serializers.ValidationError("Invalid OTP Token")
-            #     else:
-            #         raise serializers.ValidationError("OTP Token required")
-        else :
-            serializers.ValidationError("Both Username and password required")
-
-        attrs['user'] = user
-        return attrs
+########################################################################################
 
 class SignUpSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True)
@@ -125,6 +100,35 @@ class SignUpSerializer(serializers.ModelSerializer):
 
         return user
 
+class SignInSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    otp_token = serializers.CharField(max_length=6, required=False)
+    extra_kwargs = {
+        'password':{'write_only':True}
+    }
+
+    def validate(self, attrs):
+        usernm = attrs.get('username')
+        passwd = attrs.get('password')
+        if usernm and passwd:
+            user = authenticate(username=usernm, password=passwd)
+            if not user:
+                raise serializers.ValidationError("Invalid Credentials")
+            # if the the otp would be sent with the user credentials
+            # if user.enabled_2fa:
+            #     if attrs.get('otp_token'):
+            #         totp = pyotp.TOTP(user.otp_secret_key)
+            #         if not totp.verify(otp_token):
+            #             raise serializers.ValidationError("Invalid OTP Token")
+            #     else:
+            #         raise serializers.ValidationError("OTP Token required")
+        else :
+            serializers.ValidationError("Both Username and password required")
+
+        attrs['user'] = user
+        return attrs
+
 class GenerateOTPSerializer(serializers.Serializer):
         username = serializers.CharField()
 
@@ -140,18 +144,20 @@ class ValidateOTPSerializer(serializers.Serializer):
         if not attrs.get('otp_token') or not attrs.get('username'):
             raise serializers.ValidationError("OTP Token required")
 
+
 class UpdateUserInfosSerializer(serializers.ModelSerializer):
     class Meta:
         model=Player
-        fields=('username','avatar','cover',)
+        fields=['username']
+        # fields=('username','avatar','cover',)
 
     def save(self, **kwargs):
         user = self.context['user']
         player = Player.objects.get(username=user.username)
         if 'username' in self.validated_data:
             player.username = self.validated_data['username']
-        if 'avatar' in self.validated_data:
-            player.avatar = self.validated_data['avatar']
-        if 'cover' in self.validated_data:
-            player.cover = self.validated_data['cover']
+        # if 'avatar' in self.validated_data:
+        #     player.avatar = self.validated_data['avatar']
+        # if 'cover' in self.validated_data:
+        #     player.cover = self.validated_data['cover']
         player.save()
--- src/Backend/source/accounts/viewss/player_views.py
+++ src/Backend/source/accounts/viewss/player_views.py
@@ -3,26 +3,26 @@ from rest_framework import permissions
 from rest_framework import viewsets
 from ..permissions import *
 from ..serializers import *
-from django.shortcuts import get_object_or_404
-
-# from rest_framework.views import APIView, Response
-# from rest_framework import request, status
+from rest_framework import status , response
+from rest_framework.exceptions import NotFound
 
 class IsOwnerOrAdmin(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return (obj.player_profile.player == request.user or request.user.is_staff)
+        # print(f"===== DEBUG ==== : (|{obj.display_name}| == |{request.user.profile.display_name}|) is staff or user : {(request.user == obj.player or request.user.is_staff)}")
+        return (request.user == obj.player or request.user.is_staff)
 
 
 class IsOwnerOrAdminReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        # Allow GET and OPTION and HEAD requests for any user
         if request.method in permissions.SAFE_METHODS:
             return True
+        # print(f"===== DEBUG ==== : (|{obj.display_name}| == |{request.user.profile.display_name}|) is staff or user : {(request.user == obj.player or request.user.is_staff)}")
 
-        # Allow PUT and PATCH only if the user owns the object
-        return (obj.player == request.user or request.user.is_staff)
+        # return True
+
+        return (request.user == obj.player or request.user.is_staff)
 
 
 #--------------------------Players RESTFUL API ------------------------------
@@ -34,31 +34,70 @@ class IsOwnerOrAdminReadOnly(permissions.BasePermission):
 #     permission_classes = [AllowAny]
 
 class PlayerProfileViewSet(viewsets.ModelViewSet):
-    queryset = PlayerProfile.objects.all() # exlude disabled profiles with is_active from the account return to front that the profile is private
+    # exlude disabled profiles with is_active from the account return to front that the profile is private
+    queryset = PlayerProfile.objects.all()
     serializer_class = PlayerProfileSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrAdminReadOnly]
-    http_method_names = ['get', 'put', 'patch', 'head', 'options']
+    permission_classes = [AllowAny, IsOwnerOrAdminReadOnly]
+    http_method_names = ['get', 'put', 'patch', 'options']
 
-
-# class PlayerSettingsViewSet(viewsets.ModelViewSet):
-#     serializer_class = PlayerSettingsSerializer
-#     permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
-#     http_method_names = ['get', 'put', 'patch', 'head', 'options']
-
-#     def get_object(self):
-#         profile_id = self.kwargs.get('profile_id')
-#         # Ensure the settings belong to the requesting user
-#         return get_object_or_404(PlayerSettings, profile__id=profile_id, profile__user=self.request.user)
-
-
-class MatchHistoryView(viewsets.ReadOnlyModelViewSet):
+class MatchHistoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = MatchHistory.objects.all()
+    serializer_class = MatchHistorySerializer
+    permission_classes = [AllowAny]
+
+    def retrieve(self, request, pk=None):
+        try:
+            profile = PlayerProfile.objects.get(pk=pk)
+
+            serializer = self.get_serializer(profile.all_matches(), many=True)
+            return response.Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        except PlayerProfile.DoesNotExist:
+            raise NotFound("Player profile not found.")
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = PlayerProfileSerializer
+    permission_classes = [IsAuthenticated]  ## test out
+    http_method_names = ['get', 'put', 'patch', 'options']
+
+    def get_queryset(self):
+        return PlayerProfile.objects.filter(player=self.request.user)
+
+    def get_object(self):
+        try:
+            return self.get_queryset().get()
+        except PlayerProfile.DoesNotExist:
+            raise NotFound("Player profile not found.")
+        except PlayerProfile.MultipleObjectsReturned:
+            raise NotFound("Error Multiple player profile found for the user.")
+
+
+class UserSettingsViewSet(viewsets.ModelViewSet):
+    serializer_class = PlayerSettingsSerializer
+    permission_classes = [IsAuthenticated] ## test out
+    http_method_names = ['get', 'put', 'patch', 'options']
+
+    def get_queryset(self):
+        return PlayerSettings.objects.filter(id=self.request.user.id)
+
+    def get_object(self):
+        try:
+            return self.get_queryset().get()
+        except PlayerSettings.DoesNotExist:
+            raise NotFound("Player settings not found.")
+        except PlayerSettings.MultipleObjectsReturned:
+            raise NotFound("Error Multiple player settings found for the user.")
+
+
+class UserHistoryViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = MatchHistorySerializer
     permission_classes = [IsAuthenticated]
 
-
-# class UserProfileView():
-
-# class UserSettingsView()
-
-# class UserHistoryView()
+    def get_queryset(self):
+        user = self.request.user
+        try:
+            player_profile = user.profile
+            return player_profile.all_matches()
+        except PlayerProfile.DoesNotExist:
+            raise NotFound("Player profile not found.")
