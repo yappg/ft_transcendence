31299d59274944570258246c7af18334007a8150
omar-xy <otaraki@gmail.com>
2024-12-06 18:31:25 +0100

    ...

--- src/Backend/source/relations/signals.py
+++ src/Backend/source/relations/signals.py
@@ -2,21 +2,26 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Friends, FriendInvitation, Notification
 from chat.models import Message
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
 
 @receiver(post_save, sender=FriendInvitation)
 def create_friend_invitation_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(
+            sender=instance.sender,
             recipient=instance.receiver,
-            content=f'You have a new friend invitation from {instance.sender.username}'
+            message=f'You have a new friend invitation from {instance.sender.username}'
         )
 
 @receiver(post_save, sender=Friends)
 def create_friend_request_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(
+            sender = instance.friend_requester,
             recipient=instance.friend_responder,
-            content=f'You are now friends with {instance.friend_requester.username}'
+            message=f'You are now friends with {instance.friend_requester.username}'
         )
 
 # @receiver(post_save, sender=Message)
@@ -29,19 +34,19 @@ def create_friend_request_notification(sender, instance, created, **kwargs):
 #         )
 
 
-# @receiver(post_save, sender=Notification)
-# def send_notification_via_websocket(sender, instance, **kwargs):
-#     channel_layer = get_channel_layer()
-#     group_name = f'notifications_{instance.recipient.id}'
+@receiver(post_save, sender=Notification)
+def send_notification_via_websocket(sender, instance, **kwargs):
+    channel_layer = get_channel_layer()
+    group_name = f'notifications_{instance.recipient.id}'
 
-#     async_to_sync(channel_layer.group_send)(
-#         group_name,
-#         {
-#             'type': 'send_notification',
-#             'notification': {
-#                 'message': instance.message,
-#                 'created_at': instance.created_at.isoformat(),
-#                 'read': instance.read,
-#             }
-#         }
-#     )
+    async_to_sync(channel_layer.group_send)(
+        group_name,
+        {
+            'type': 'send_notification',
+            'notification': {
+                'message': instance.message,
+                'created_at': instance.created_at.isoformat(),
+                'read': instance.read,
+            }
+        }
+    )
