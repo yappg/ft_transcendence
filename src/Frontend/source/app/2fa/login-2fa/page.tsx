b506ec410ccb5d693800980220e3459625129a07
Noureddine Akebli <nakebli@e1r10p13.1337.ma>
2024-12-07 01:58:52 +0100

    changed rout reotection logic

--- src/Frontend/source/app/2fa/login-2fa/page.tsx
+++ src/Frontend/source/app/2fa/login-2fa/page.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { MyButton } from '@/components/generalUi/Button';
 import { sendOtp } from '@/services/fetch-otp';
 import { useAuth } from '@/context/AuthContext';
-import withAuth from '@/context/requireAhuth';
 import { toast } from '@/hooks/use-toast';
 
 const Login2fa = () => {
@@ -36,7 +35,7 @@ const Login2fa = () => {
   return (
     <div className="flex size-full flex-col items-center justify-center gap-10 transition-all">
       <div className="flex flex-col items-start justify-center transition-all">
-        <h1 className="font-dayson text-3xl text-black opacity-80 transition-all duration-300 sm:text-5xl dark:text-white">
+        <h1 className="font-dayson text-3xl text-black opacity-80 transition-all duration-300 dark:text-white sm:text-5xl">
           2FA Code Required
         </h1>
       </div>
@@ -44,7 +43,7 @@ const Login2fa = () => {
       <div className="flex items-end justify-end transition-all duration-300">
         <MyButton
           onClick={handleClick}
-          className=" font-coustard h-[68px] w-[201px] rounded-lg text-[24px]
+          className=" h-[68px] w-[201px] rounded-lg font-coustard text-[24px]
       font-black transition-all duration-300 md:h-[88px] md:w-[301px] md:text-[36px]"
         >
           {myString}
@@ -54,4 +53,4 @@ const Login2fa = () => {
   );
 };
 
-export default withAuth(Login2fa, false);
+export default Login2fa;
--- src/Frontend/source/app/2fa/signup-2fa/page.tsx
+++ src/Frontend/source/app/2fa/signup-2fa/page.tsx
@@ -6,7 +6,6 @@ import { InputOTPDemo } from '@/components/2fa/InputOTPDemo';
 import { MyButton } from '@/components/generalUi/Button';
 import { useQRCode } from 'next-qrcode';
 import { fetchQrCode, sendOtp } from '@/services/fetch-otp';
-import withAuth from '@/context/requireAhuth';
 import { useAuth } from '@/context/AuthContext';
 import { useRouter } from 'next/navigation';
 
@@ -36,16 +35,16 @@ const Signup2fa = () => {
           return router.push('/home');
         }
       };
-      valid()
+      valid();
     }
   };
   return (
     <div className="relative flex size-auto flex-col items-center justify-center gap-4 md:h-[90%] md:w-[85%]">
-      <h1 className=" font-dayson flex items-center justify-center text-[40px] text-white transition-all duration-300">
+      <h1 className=" flex items-center justify-center font-dayson text-[40px] text-white transition-all duration-300">
         activate 2FA
       </h1>
       {isLoading ? (
-        <h1 className="size-[200px] flex justify-center items-center font-dayson border border-white-crd rounded-md text-[30px] text-gray-600">
+        <h1 className="flex size-[200px] items-center justify-center rounded-md border border-white-crd font-dayson text-[30px] text-gray-600">
           Loading...
         </h1>
       ) : (
@@ -65,13 +64,10 @@ const Signup2fa = () => {
       )}
       <InputOTPDemo value={value} setValue={setValue} />
       <div className="flex flex-col items-center justify-center gap-[20px] md:w-full md:gap-[40px]">
-        <MyButton
-          onClick={handleClick}
-          className="min-w-[120px] disabled:opacity-50"
-        >
+        <MyButton onClick={handleClick} className="min-w-[120px] disabled:opacity-50">
           {myString}
         </MyButton>
-        <Link href="/home" className="font-poppins text-[#284F50] text-[10px]">
+        <Link href="/home" className="font-poppins text-[10px] text-[#284F50]">
           Skip this step
         </Link>
       </div>
@@ -79,6 +75,4 @@ const Signup2fa = () => {
   );
 };
 
-// export default Signup2fa;
-
-export default withAuth(Signup2fa, true);
+export default Signup2fa;
