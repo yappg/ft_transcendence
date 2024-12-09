d586641f592418280696f000189f9bca393f1d81
rambeau <bennixmehdi0@gmail.com>
2024-12-09 16:57:31 +0100

    still working on the elo system

--- src/Backend/source/accounts/views/functionViews.py
+++ src/Backend/source/accounts/views/functionViews.py
@@ -4,6 +4,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from ..models import PlayerProfile
 from ..serializers.functionSerlizers import *
+from ..serializers.userManagmentSerlizers import PlayerProfileSerializer
+
+from django.core.cache import cache
 
 class SearchUsersView(APIView):
     permission_classes = [IsAuthenticated]
@@ -18,3 +21,22 @@ class SearchUsersView(APIView):
         serializer = SearchUsersSerializer(players, many=True)
 
         return Response({'count': players.count(), 'results': serializer.data}, status=status.HTTP_200_OK)
+
+
+# class PlayerProfileSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = PlayerProfile
+#         fields = ['display_name', 'elo_rating', 'level', 'xp', 'games_played', 'games_won', 'games_loss', 'win_ratio']
+
+class LeaderboardView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        leaderboard = cache.get('top_100_leaderboard')
+        if not leaderboard:
+            top_players = PlayerProfile.objects.all().order_by('-elo_rating')[:100]
+            serializer = PlayerProfileSerializer(top_players, many=True)
+            leaderboard = serializer.data
+            cache.set('top_100_leaderboard', leaderboard, timeout=60*5)  # Cache for 5 minutes
+        return Response(leaderboard, status=200)
+
