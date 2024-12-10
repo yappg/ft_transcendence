583bd7f04f44afd44f530a75c872e8204f857fdf
ElmehdiBennix <bennixmehdi@gmail.com>
2024-12-10 21:58:15 +0100

    leveling up working fine

--- src/Backend/source/accounts/serializers/functionSerlizers.py
+++ src/Backend/source/accounts/serializers/functionSerlizers.py
@@ -2,18 +2,18 @@ from rest_framework import serializers
 from ..models import *
 
 class SearchUsersSerializer(serializers.ModelSerializer):
-    username = serializers.SerializerMethodField()
+    # username = serializers.SerializerMethodField()
 
     class Meta:
         model = PlayerProfile
         fields = [
             'id' ,
-            'username',
+            # 'username',
             'display_name',
             'avatar',
             'is_online'
         ]
         read_only_fields = [ '__all__']
 
-    def get_username(self, obj):
-        return obj.player.username
+    # def get_username(self, obj):
+    #     return obj.player.username
--- src/Backend/source/accounts/views/functionViews.py
+++ src/Backend/source/accounts/views/functionViews.py
@@ -15,7 +15,7 @@ class SearchUsersView(APIView):
         search_term = request.query_params.get('search', '')
 
         players = PlayerProfile.objects.filter(
-            player__username__istartswith=search_term
+            display_name__istartswith=search_term
         )
 
         serializer = SearchUsersSerializer(players, many=True)
@@ -28,15 +28,14 @@ class SearchUsersView(APIView):
 #         model = PlayerProfile
 #         fields = ['display_name', 'elo_rating', 'level', 'xp', 'games_played', 'games_won', 'games_loss', 'win_ratio']
 
-class LeaderboardView(APIView):
-    permission_classes = [IsAuthenticated]
+# class LeaderboardView(APIView):
+#     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
-        leaderboard = cache.get('top_100_leaderboard')
-        if not leaderboard:
-            top_players = PlayerProfile.objects.all().order_by('-elo_rating')[:100]
-            serializer = PlayerProfileSerializer(top_players, many=True)
-            leaderboard = serializer.data
-            cache.set('top_100_leaderboard', leaderboard, timeout=60*5)  # Cache for 5 minutes
-        return Response(leaderboard, status=200)
-
+#     def get(self, request):
+#         leaderboard = cache.get('top_100_leaderboard')
+#         if not leaderboard:
+#             top_players = PlayerProfile.objects.all().order_by('-elo_rating')[:100]
+#             serializer = PlayerProfileSerializer(top_players, many=True)
+#             leaderboard = serializer.data
+#             cache.set('top_100_leaderboard', leaderboard, timeout=60*5)  # Cache for 5 minutes
+#         return Response(leaderboard, status=200)
