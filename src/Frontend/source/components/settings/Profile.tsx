39f6d92defab83a268ecd3fa001ec7b3791f908d
Meryeme Mesbahi <mmesbahi@e1r9p13.1337.ma>
2024-12-10 18:52:31 +0100

    add profile form

--- src/Frontend/source/components/settings/Profile.tsx
+++ src/Frontend/source/components/settings/Profile.tsx
@@ -5,17 +5,28 @@ import { IconBrandCampaignmonitor } from '@tabler/icons-react';
 import { IconUsersMinus } from '@tabler/icons-react';
 import { IconLock } from '@tabler/icons-react';
 import { IconEdit } from '@tabler/icons-react';
-
-const Profile = () => {
-  const [clicked, setClicked] = React.useState(0);
-  const Player = {
-    username: 'player',
-    email: 'example@gmail.com',
-    password: 'password',
+import { useState } from 'react';
+import { Button } from '../ui/button';
+import { set } from 'zod';
+const Profile = ({
+  Profile,
+  setProfile,
+} :
+{
+  Profile: {
+    username: string;
+    email: string;
+    oldPassword: string;
   };
-  function handleClick(id:number) {
-    setClicked(id);
-  }
+  setProfile: (value: any) => void;
+}) => {
+  const [newProfile, setNewProfile] = useState({
+    username: Profile.username,
+    email: Profile.email,
+    oldPass: '',
+    newPass: '',
+  });
+
   return (
     <Content>
       <div className="w-full h-[95%] flex flex-col items-center justify-center">
@@ -27,11 +38,87 @@ const Profile = () => {
             </h1>
           </div>
         </div>
-        <div className="h-[90%] w-full ">
+        <div className="w-full h-[90%] px-8 py-12 flex flex-col items-start justify-start gap-12">
+          <div className="w-full h-fit flex flex-col items-start justify-center gap-6">
+            <h1 className="text-white text-xl font-dayson">Email</h1>
+            <Input
+              value={newProfile.email}
+              onChange={(e) => {
+                setNewProfile({ ...newProfile, email: e.target.value });
+              }}
+              placeholder="New mail..."
+              className="bg-[#0000003D] rounded-[50px] w-[400px] text-white  placeholder:opacity-55"
+            />
+          </div>
+          <div className="w-full h-fit flex flex-col items-start justify-center gap-6">
+            <h1 className="text-white text-xl font-dayson">Username</h1>
+            <Input
+              value={newProfile.username}
+              onChange={(e) => {
+                setNewProfile({ ...newProfile, username: e.target.value });
+              }}
+              placeholder="New mail..."
+              className="bg-[#0000003D] rounded-[50px] w-[400px] text-white  placeholder:opacity-55"
+            />
+          </div>
+          <div className="w-full h-fit flex flex-col items-start justify-center gap-6">
+            <h1 className="text-white text-xl font-dayson">Password</h1>
+            <div className="flex flex-col items-start justify-center gap-2">
+              <h1 className="text-white text-[14px] font-dayson">
+                Old Password
+              </h1>
+            <Input
+              value={newProfile.oldPass}
+              type="password"
+              onChange={(e) => {
+                setNewProfile({ ...newProfile, oldPass: e.target.value });
+              }}
+              placeholder="Write old password..."
+              className="bg-[#0000003D] rounded-[50px] w-[400px] text-white  placeholder:opacity-55"
+            />
+            </div>
+            <div className="flex flex-col items-start justify-center gap-2">
+              <h1 className="text-white text-[14px] font-dayson">
+                  New Password
+                </h1>
+              <Input
+                value={newProfile.newPass}
+                type="password"
+                onChange={(e) => {
+                  setNewProfile({ ...newProfile, newPass: e.target.value });
+                }}
+                placeholder="Write new password..."
+                className="bg-[#0000003D] rounded-[50px] w-[400px] text-white  placeholder:opacity-55"
+              />
+            </div>
+          </div>
+          <Button
+            onClick={() => {
+              if (newProfile.oldPass != Profile.oldPassword) {
+                console.log('Old password is wrong');
+              } else 
+              {
+                setProfile({ ...Profile, oldPassword: newProfile.newPass, email: newProfile.email, username: newProfile.username });
+                setNewProfile({ ...newProfile, oldPass: '', newPass: '' });
+              }
+            }}
+            className="bg-[#fff] rounded-[13px] w-[400px] text-black placeholder:opacity-55"
+          >
+            Save
+          </Button>
+        </div>
+      </div>
+    </Content>
+  );
+};
+export default Profile;
+
+{
+  /* <div className="h-[90%] w-full ">
           <div className="h-1/4 w-full ">
             <div
               className="h-[50%] w-[100%] flex items-center justify-start flex-row gap-8"
-              onClick={handleClick}
+              onClick={() => handleClick(1)}
             >
               <div className="rounded-[50px] h-full w-[130px] ml-[-40px] flex justify-end items-center bg-[#FFFFFF4F] pr-2 ">
                 <IconBrandCampaignmonitor stroke={2} className="text-[#00000099] size-12" />
@@ -39,35 +126,41 @@ const Profile = () => {
               <h1 className="text-[25px] text-[#00000099] font-coustard">Email</h1>
             </div>
             <div
-              className={`${clicked ? 'flex' : 'hidden'} h-[50%] w-[100%] items-start justify-center gap-40 flex flex-row border-b-2`}
+              className={`${clicked === 1 ? 'flex' : 'hidden'} h-[50%] w-[100%] items-start justify-center gap-40 flex flex-row border-b-2`}
             >
               <div className="h-full w-[50%] flex items-center justify-center">
                 <div className="h-[50px] w-[400px] flex items-center justify-center border-2 border-white opacity-40 rounded-[50px]">
-                  {Player.email}
+                  {Profile.email}
                 </div>
               </div>
               <div className="h-full w-[50%] flex items-center justify-center">
                 <Input
+                  value={Profile.email}
+                  onChange={(e) => {
+                    setProfile({ ...Profile, email: e.target.value });
+                  }}
                   placeholder="New mail..."
-                  className="bg-[#0000003D] rounded-[50px] w-[400px] text-white text-opacity-40 placeholder:opacity-55"
+                  className="bg-[#0000003D] rounded-[50px] w-[400px] text-white  placeholder:opacity-55"
                 />
               </div>
             </div>
           </div>
           <div className="h-1/4 w-full">
-          <div
+            <div
               className="h-[50%] w-[100%] flex items-center justify-start flex-row gap-8"
-              onClick={handleClick}
+              onClick={() => handleClick(2)}
             >
               <div className="rounded-[50px] h-full w-[130px] ml-[-40px] flex justify-end items-center bg-[#FFFFFF4F] pr-2 ">
                 <IconUsersMinus stroke={2} className="text-[#00000099] size-12" />
               </div>
-              <h1 className="text-[25px] text-[25px] text-[#00000099] font-coustard">Username</h1>
+              <h1 className="text-[25px] text-[#00000099] font-coustard">Username</h1>
             </div>
-            <div className="h-[50%] w-[100%] items-start justify-center gap-40 flex flex-row">
+            <div
+              className={`${clicked === 2 ? 'flex' : 'hidden'} h-[50%] w-[100%] items-start justify-center gap-40 flex flex-row`}
+            >
               <div className="h-full w-[50%] flex items-center justify-center">
                 <div className="h-[50px] w-[400px] flex items-center justify-center border-2 border-white opacity-40 rounded-[50px]">
-                  {Player.username}
+                  {Profile.username}
                 </div>
               </div>
               <div className="h-full w-[50%] flex items-center justify-center">
@@ -79,16 +172,18 @@ const Profile = () => {
             </div>
           </div>
           <div className="h-1/4 w-full">
-          <div
+            <div
               className="h-[50%] w-[100%] flex items-center justify-start flex-row gap-8"
-              onClick={handleClick}
+              onClick={() => handleClick(3)}
             >
-              <div className="rounded-[50px] h-full w-[130px] ml-[-40px] flex justify-end items-center bg-[#FFFFFF4F] pr-2 ">
+              <div className="rounded-[50px] h-full w-[130px] ml-[-40px] flex justify-end items-center bg-[#FFFFFF4F] pr-2">
                 <IconLock stroke={2} className="text-[#00000099] size-12" />
               </div>
               <h1 className="text-[25px] text-[#00000099] font-coustard">Password</h1>
             </div>
-            <div className="h-[50%] w-[100%] items-start justify-center gap-40 flex flex-row">
+            <div
+              className={`${clicked === 3 ? 'flex' : 'hidden'} h-[50%] w-[100%] items-start justify-center gap-40 flex flex-row`}
+            >
               <div className="h-full w-[50%] flex items-center justify-center">
                 <Input
                   placeholder="Old password..."
@@ -104,9 +199,5 @@ const Profile = () => {
             </div>
           </div>
           <div className="h-1/4 w-full"></div>
-        </div>
-      </div>
-    </Content>
-  );
-};
-export default Profile;
+        </div> */
+}
