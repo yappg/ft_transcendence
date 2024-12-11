d44a375520b08f76c620098ee07197c8ce9a0dfd
Meryeme Mesbahi <mmesbahi@e1r9p12.1337.ma>
2024-12-11 22:09:35 +0100

    fix the graph bug

--- src/Frontend/source/app/(main)/Profile/page.tsx
+++ src/Frontend/source/app/(main)/Profile/page.tsx
@@ -4,7 +4,11 @@ export default function page() {
   return (
     <div className="col-span-10 col-start-2 row-span-8 row-start-2 grid grid-cols-[1fr] py-4 pl-6">
       <div className="costum-little-shadow size-full overflow-hidden rounded-[50px]">
-        <div className="col-start-1 col- flex items-center justify-center w-full h-[45%]">
+        <div className="col-start-1 col- flex items-center justify-center w-full h-[45%]" style={{
+          backgroundImage: 'url(/bg-profile.svg)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}>
           <UserInfo />
         </div>
         <div className="flex items-center justify-center w-full h-[55%]">
