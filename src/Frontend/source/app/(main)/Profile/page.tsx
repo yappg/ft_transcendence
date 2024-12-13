7d254af412800637aa1a4547a1173a6912d652ae
Meryeme Mesbahi <mmesbahi@e1r9p13.1337.ma>
2024-12-13 02:55:08 +0100

    fix the responsive of the main layout

--- src/Frontend/source/app/(main)/Profile/page.tsx
+++ src/Frontend/source/app/(main)/Profile/page.tsx
@@ -2,13 +2,19 @@ import UserInfo from '@/components/Profile/UserInfo';
 import UserSummary from '@/components/Profile/UserSummary';
 export default function page() {
   return (
-    <div className="col-span-10 col-start-2 row-span-8 row-start-2 grid grid-cols-[1fr] py-4 pl-6">
-      <div className="costum-little-shadow size-full overflow-hidden rounded-[50px]">
-        <div className="col-start-1 col- flex items-center justify-center w-full h-[45%]" style={{
-          backgroundImage: 'url(/bg-profile.svg)',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}>
+    <div className="lg:col-span-10 lg:col-start-2 col-span-full col-start-1 row-span-8 row-start-2 grid grid-cols-[1fr] lg:py-4 lg:pl-6">
+      <div className="costum-little-shadow size-full overflow-hidden lg:rounded-[50px] ">
+        <div className="relative col-start-1 col- flex items-center justify-center w-full h-[45%]">
+          <div
+            className="absolute h-full w-full -z-0"
+            style={{
+              backgroundImage: "url('/bg-profile.svg')",
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat',
+              filter: 'blur(10px)',
+            }}
+          ></div>
           <UserInfo />
         </div>
         <div className="flex items-center justify-center w-full h-[55%]">
