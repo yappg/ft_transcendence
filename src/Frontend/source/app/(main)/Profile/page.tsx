f9c69eca19ac5c54bb005d225ea01c72072931b4
Meryeme Mesbahi <mmesbahi@e1r9p13.1337.ma>
2024-12-15 15:06:29 +0100

    fixing the responsive of the Profile page

--- src/Frontend/source/app/(main)/Profile/page.tsx
+++ src/Frontend/source/app/(main)/Profile/page.tsx
@@ -2,9 +2,9 @@ import UserInfo from '@/components/Profile/UserInfo';
 import UserSummary from '@/components/Profile/UserSummary';
 export default function page() {
   return (
-    <div className="lg:col-span-10 lg:col-start-2 col-span-full col-start-1 row-span-8 row-start-2 grid grid-cols-[1fr] lg:py-4 lg:pl-6">
-      <div className="costum-little-shadow size-full overflow-hidden lg:rounded-[50px] gap-8 ">
-        <div className="relative col-start-1 col- flex items-center justify-center w-full sm:h-[40%] h-[50%]">
+    <div className="lg:col-span-10 lg:col-start-2 col-span-full col-start-1 row-span-8 row-start-2 grid grid-cols-[1fr] md:py-4 md:pl-6 overflow-auto">
+      <div className="costum-little-shadow size-full overflow-hidden lg:rounded-[50px] gap-8">
+        <div className="relative col-start-1 col- flex items-center justify-center w-full sm:h-[40%] h-[50%] min-h-[500px]">
           <div
             className="absolute h-full w-full -z-0"
             style={{
