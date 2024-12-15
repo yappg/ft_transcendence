1ddcc9882271268ded59d3090bf06feedc74c049
Kadigh53 <abdlkadraoutem@gmail.com>
2024-12-15 21:19:51 +0100

    save changes

--- src/Backend/source/game/consumers.py
+++ src/Backend/source/game/consumers.py
@@ -1,5 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
 from django.contrib.auth.models import AnonymousUser
 from .services import GameService, LeaderboardService
 from .models import Game, PlayerProfile
@@ -18,13 +19,22 @@ class GameConsumer(AsyncWebsocketConsumer):
         try:
             await self.accept()
             self.redis = get_redis_connection("default")
+            #self.redis = get_redis_connection("matchmaking")
             self.groupe_name = f'game_{user.username}'
             self.channel_layer.group_add(self.groupe_name, self.channel_layer)
+            self.add_player_to_queue(user.id)
         except:
             await self.close()
             return
+
+    @database_sync_to_async
     async def add_player_to_queue(self, user_id):
-        
+        self.redis.rpush('game_queue', user_id)
+
+    # @database_sync_to_async
+    # async def remove_player_from_queue(self):
+    #     userID = self.redis.lpop('game_queue')
+
     async def receive(self, text_data):
         pass
 
