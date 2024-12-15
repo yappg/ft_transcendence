c3686e8703fd03cee7c649664da62a1ff83457b0
Meryeme Mesbahi <mmesbahi@e1r9p13.1337.ma>
2024-12-15 22:48:57 +0100

    fix the responsive of the layout and starting the settings

--- src/Frontend/source/components/Profile/UserInfo.tsx
+++ src/Frontend/source/components/Profile/UserInfo.tsx
@@ -11,9 +11,9 @@ const UserInfo = () => {
     winRate: 50,
   };
   return (
-    <div className="size-full flex items-center justify-start px-10 absolute z-99 md:flex-row flex-col">
-      <div className="md:h-full md:w-[80%] w-full h-[80%] flex sm:flex-row flex-col">
-        <div className="flex md:items-end items-center justify-center sm:w-[500px] sm:h-full pb-6 w-full h-[75%] pt-8 sm:pt-0">
+    <div className="size-full flex items-center justify-start px-10 absolute z-99 lg:flex-row flex-col">
+      <div className="lg:h-full lg:w-[80%] w-full h-[80%] flex sm:flex-row flex-col">
+        <div className="flex lg:items-end items-center justify-center sm:w-[500px] sm:h-full pb-6 w-full h-[75%] pt-0">
           <img
             src="/ProfilePicture.svg"
             alt="Profile"
@@ -21,7 +21,7 @@ const UserInfo = () => {
           />
         </div>
 
-        <div className="flex items-start md:justify-end sm:justify-center sm:w-[700px] h-full flex-col p-10 w-full">
+        <div className="flex sm:items-start items-center lg:justify-end sm:justify-center sm:w-[700px] h-full flex-col p-10 w-full">
           <div className="gap-10">
             <h1 className="2xl:text-[50px] xl:text-[45px] lg:text-[35px] md:text-[30px] text-[20px] font-dayson text-white ">
               {user.name}
@@ -37,17 +37,17 @@ const UserInfo = () => {
           </div>
         </div>
       </div>
-      <div className="ml-auto flex md:flex-col flex-row items-end justify-between text-white md:h-full md:w-[20%] w-full h-[20%] md:py-8 py-5">
-        <div className="md:gap-4 md:w-[150px] 2xl:w-[200px] flex md:flex-col sm:w-[90%] sm:h-full">
-          <div className="sm:w-1/3 md:w-full 2xl:text-[20px] xl:text-[15px] font-dayson text-white sm:flex sm:gap-3 md:gap-0 sm:flex-col sm:items-center md:flex-row sm:border-r-2 md:border-none">
+      <div className="ml-auto flex lg:flex-col flex-row items-end justify-between text-white lg:h-full lg:w-[20%] w-full h-[20%] lg:py-8 py-5">
+        <div className="lg:gap-4 lg:w-[150px] 2xl:w-[200px] flex lg:flex-col w-[90%] sm:h-full h-[70%]">
+          <div className="w-1/3 lg:w-full 2xl:text-[20px] sm:text-[15px] text-[10px] font-dayson text-white flex gap-3 lg:gap-0 flex-col items-center lg:flex-row border-r-2 lg:border-none">
             <h1>Total Wins :</h1>
             <h2>{user.totalWins}</h2>
           </div>
-          <div className="sm:w-1/3 md:w-full 2xl:text-[20px] xl:text-[15px] font-dayson text-white sm:flex sm:gap-3 md:gap-0 sm:flex-col sm:items-center md:flex-row sm:border-r-2 md:border-none">
+          <div className="w-1/3 lg:w-full 2xl:text-[20px] sm:text-[15px] text-[10px] font-dayson text-white flex gap-3 lg:gap-0 flex-col items-center lg:flex-row border-r-2 lg:border-none">
             <h1>Total Bets :</h1>
             <h2>{user.totalBets}</h2>
           </div>
-          <div className="sm:w-1/3 md:w-full 2xl:text-[20px] xl:text-[15px] font-dayson text-white sm:flex sm:gap-3 md:gap-0 sm:flex-col sm:items-center md:flex-row">
+          <div className="w-1/3 lg:w-full 2xl:text-[20px] sm:text-[15px] text-[10px] font-dayson text-white flex gap-3 lg:gap-0 flex-col items-center lg:flex-row">
             <h1>Wins Rate :</h1>
             <h1>{user.winRate}%</h1>
           </div>
@@ -56,7 +56,7 @@ const UserInfo = () => {
           href="/settings?field=profile"
           className="text-black bg-[#B7B7B7] 2xl:size-[50px] md:size-[45px] rounded-[10px]"
         >
-          <HiOutlinePencilSquare className="2xl:size-[50px] sm:size-[45px]" />
+          <HiOutlinePencilSquare className="2xl:size-[50px] sm:size-[45px] size-[30px]" />
         </Link>
       </div>
     </div>
