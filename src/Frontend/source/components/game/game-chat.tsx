6861fbee3b7bde9743f8d97aaba80a5cf03588de
Noureddine Akebli <nakebli@e2r12p2.1337.ma>
2024-12-15 23:33:27 +0100

    handled racket collision and finished score bar design

--- src/Frontend/source/components/game/game-chat.tsx
+++ src/Frontend/source/components/game/game-chat.tsx
@@ -1,45 +1,46 @@
+/* eslint-disable @next/next/no-img-element */
+'use client';
+
 import Link from 'next/link';
 import { IoIosArrowBack } from 'react-icons/io';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
-import { useState } from 'react';
+import { useUser, User } from '@/context/GlobalContext';
+// import { useState } from 'react';
 
-const [playerScore, setplayerScore] = useState();
+// const [playerScore, setplayerScore] = useState();
 
-export const PlayerScore = ({player, oponent}: {player:string, oponent:string}) => {
-    return (
-        <div className="flex">
-            <div>
-                <Avatar>
-                    <AvatarImage src="/logo.svg" alt="@shadcn" />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <div className='flex flex-col'>
-                    <div>
-                        {player}
-                    </div>
-                    <div>
-                        {score}
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
-
-export const GameChat = () => {
+const PlayerScore = ({ player, isme }: { player: User; isme: boolean }) => {
   return (
-    <div className="flex flex-col justify-around">
-      <div>
-        <Link
-          href={'#'}
-          className="text-white text-[48px] font-dayson flex items-center justify-start w-[400px] h-[60px]"
-        >
-          <IoIosArrowBack /> <span>Game Arena</span>
-        </Link>
-        <div className="bg-red-500 w-full h-60">
-
-        </div>
+    <div className={`flex h-4/5 w-2/5 items-center ${isme ? '' : 'justify-end'}`}>
+      <Avatar className={`h-full w-1/2 ${isme ? '' : 'order-3'}`}>
+        <AvatarImage src="/Avatar.svg" alt="avatar" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <div className="flex flex-col font-dayson text-[34px] dark:text-white">
+        <div>{player.username}</div>
+        <div className={`w-full ${isme ? '' : 'text-end'}`}>{'18'}</div>
       </div>
     </div>
   );
 };
+
+const GameChat = () => {
+  const { user } = useUser();
+  return (
+    <div className="flex flex-col items-center justify-around gap-8">
+      <Link
+        href={'#'}
+        className="flex h-[60px] w-full items-center justify-start font-dayson text-[48px] dark:text-white"
+      >
+        <IoIosArrowBack /> <span>Game Arena</span>
+      </Link>
+      <div className="flex h-[170px] w-full items-end justify-between">
+        <PlayerScore player={user || ({} as User)} isme={true} />
+        <img src="/logo.svg" alt="logo" className="h-full" />
+        <PlayerScore player={user || ({} as User)} isme={false} />
+      </div>
+    </div>
+  );
+};
+
+export default GameChat;
