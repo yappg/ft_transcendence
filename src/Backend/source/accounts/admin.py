4bb4b0afb3de41264c81f7dac8a80855fb9bad63
Rambeau <bennixmehdi0@gmail.com>
2024-12-17 04:40:10 +0100

    fixing admin pannel issue

--- src/Backend/source/accounts/admin.py
+++ src/Backend/source/accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 
+# TODO errors when searching admin pannel
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'enabled_2fa', 'verified_otp')
@@ -12,10 +13,22 @@ class PlayerProfileAdmin(admin.ModelAdmin):
     search_fields = ('player__username', 'display_name')
     list_filter = ('level', 'is_online')
 
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 @admin.register(PlayerSettings)
 class PlayerSettingsAdmin(admin.ModelAdmin):
     list_display = ('player_profile', 'private_profile', 'notifications_enabled')
     search_fields = ('player_profile__player__username' ,'player_profile__display_name')
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 @admin.register(MatchHistory)
 class MatchHistoryAdmin(admin.ModelAdmin):
@@ -23,12 +36,25 @@ class MatchHistoryAdmin(admin.ModelAdmin):
     search_fields = ('player1__display_name', 'player2__display_name')
     list_filter = ('result', 'map_played', 'date')
 
+
 @admin.register(Achievement)
 class AchivementAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'xp_gain')
     search_fields = ('name', 'xp_gain')
 
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 @admin.register(PlayerAchievement)
 class PlayerAchievementAdmin(admin.ModelAdmin):
     list_display = ('achievement', 'player', 'gained', 'date_earned')
-    search_fields = ('achievement', 'gained', 'player__display_name')
+    search_fields = ('achievement__name', 'gained', 'player__display_name')
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
