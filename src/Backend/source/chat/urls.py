f92a78f2ae4e00813c2b9ee933dc23d2c4f2d2dd
Rambeau <bennixmehdi0@gmail.com>
2024-12-23 09:02:09 +0100

    fixed relations still work to be done in endpoints

--- src/Backend/source/chat/urls.py
+++ src/Backend/source/chat/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import ChatListView
 
 urlpatterns = [
-    path('ch/', ChatView.as_view(), name='create_chat'),
     path('list/', ChatListView.as_view(), name='list_chat'),
     path('<int:chatId>/messages/', ChatMessagesView.as_view(), name='chat_messages'),
 ]
