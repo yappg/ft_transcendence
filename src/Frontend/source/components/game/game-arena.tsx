f8ac82ae5e042df3db5d9041b05c1baf04e30c57
Nourdine <akeblinourdine14@gmail.com>
2024-12-24 22:34:09 +0100

    fixed some responsive bugs and online game structure is up

--- src/Frontend/source/components/game/game-arena.tsx
+++ src/Frontend/source/components/game/game-arena.tsx
@@ -2,25 +2,36 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 'use client';
 
-import { LocalGameManager, PixiManager } from '@/components/game/pixi';
+import { LocalGameManager, OnlineGameManager, PixiManager } from '@/components/game/pixi';
 import { useGame } from '@/context/GameContext';
 import React, { useRef, useEffect } from 'react';
+import socketManager from './socket-manager';
 
 const GameTable = ({ mode, map }: { map: string; mode: string }) => {
   const canvasContainerRef = useRef<HTMLDivElement | null>(null);
   const game = useGame();
 
   const gameManagerRef = useRef<PixiManager | null>(null);
+  const socketRef = useRef<WebSocket | null>(null);
 
   useEffect(() => {
-    if (canvasContainerRef.current) {
-      gameManagerRef.current = new LocalGameManager(
-        canvasContainerRef.current,
-        `/${map}.png`,
-        game
-      );
+    if (mode.indexOf('local') !== -1) {
+      if (canvasContainerRef.current) {
+        gameManagerRef.current = new LocalGameManager(
+          canvasContainerRef.current,
+          `/${map}.png`,
+          game
+        );
+      }
+    } else {
+      if (canvasContainerRef.current) {
+        gameManagerRef.current = new OnlineGameManager(
+          canvasContainerRef.current,
+          `/${map}.png`,
+          game
+        );
+      }
     }
-
     return () => {
       if (gameManagerRef.current?.app) {
         gameManagerRef.current.app.destroy(true);
