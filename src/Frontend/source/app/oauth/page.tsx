57250ee52ff836faae24733a73cdc1e192018f21
omar-xy <ortaraki@gmail.com>
2024-12-26 15:06:23 +0100

    create oauth page component

---
+++ src/Frontend/source/app/oauth/page.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable tailwindcss/no-custom-classname */
+'use client';
+import React from 'react';
+
+export default function Oauth(){
+
+    return (
+        
+        <div>HELLO this oauth<div>
+    )
+};
