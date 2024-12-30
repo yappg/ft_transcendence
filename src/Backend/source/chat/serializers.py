00c7e19aaf8e1f7e09d5555035f3402b3be9c9c0
omar-xy <otaraki@student.1337.ma>
2024-12-31 00:31:06 +0100

    - Enhanced ChatRoomSerializer to include fields for blocked status and who blocked whom. - Improved chat message handling in frontend, including blocking/unblocking users and updating chat partner references.

--- src/Backend/source/chat/serializers.py
+++ src/Backend/source/chat/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import ChatRoom, Message
 from accounts.models import Player, PlayerProfile
+from relations.models import BlockedUsers
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = serializers.StringRelatedField()
@@ -12,10 +13,14 @@ class MessageSerializer(serializers.ModelSerializer):
 class ChatRoomSerializer(serializers.ModelSerializer):
     last_message = serializers.SerializerMethodField()
     receiver = serializers.SerializerMethodField()
+    is_blocked = serializers.SerializerMethodField()
+    blocked_by = serializers.SerializerMethodField()
 
     class Meta:
         model = ChatRoom
-        fields = ['id', 'created_at', 'last_message', 'receiver']
+        fields = ['id', 'created_at', 'last_message', 'receiver', 'is_blocked', 'blocked_by']
+    
+        
     
     def get_last_message(self, obj):
         last_message = obj.messages.order_by('-send_at').first()
@@ -26,7 +31,6 @@ class ChatRoomSerializer(serializers.ModelSerializer):
     def get_receiver(self, obj):
         user = self.context.get('request').user
         receivers = obj.senders.exclude(id=user.id)
-    
         if receivers.exists():
             receiver = receivers.first()
             return {
@@ -35,3 +39,21 @@ class ChatRoomSerializer(serializers.ModelSerializer):
                 'avatar': receiver.profile.avatar.url if receiver.profile.avatar else None
             }
         return None
+
+    def get_is_blocked(self, obj):
+        user = self.context.get('request').user
+        obj_receiver = obj.senders.exclude(id=user.id).first()
+        blocked = BlockedUsers.objects.get(user=user)
+        if blocked.is_blocked(obj_receiver):
+            return True
+        return False
+
+    def get_blocked_by(self, obj):
+        user = self.context.get('request').user
+        other = obj.senders.exclude(id=user.id).first()
+        
+        # here I need to check if the other user is blocked by the user
+        blocked_by = BlockedUsers.objects.get(user=other)
+        if blocked_by.is_blocked(user):
+            return True
+        return False
