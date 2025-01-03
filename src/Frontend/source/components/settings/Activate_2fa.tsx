24cbcd0ff163ae3cc417fe8ab6730b0e0aaf820c
Meryeme Mesbahi <mmesbahi@e3r3p12.1337.ma>
2025-01-03 11:39:38 +0100

    Refactor settings components,  Updated 2FA activation logic and Modified service methods for user settings and password updates

--- src/Frontend/source/components/settings/Activate_2fa.tsx
+++ src/Frontend/source/components/settings/Activate_2fa.tsx
@@ -1,36 +1,49 @@
+'use client'
 import { Switch } from '@/components/ui/switch';
+import SettingsServices from '@/services/settingsServices';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
 interface TwoFactorProps {
-  profileState: {
-    avatar: string;
-    cover: string;
-    profileError: string;
-    coverError: string;
-    username: string;
-    display_name: string;
-    password: string;
-    NewPassword: string;
-    is_private: boolean;
-  };
-  setProfileState: React.Dispatch<React.SetStateAction<any>>;
+  twoFactor: boolean;
+  setEnabled2fa: (enabled: boolean) => void;
 }
 
-export const Activate_2fa = ({profileState, setProfileState}: TwoFactorProps) => {
+export const Activate_2fa: React.FC<TwoFactorProps> = ({ twoFactor, setEnabled2fa }) => {
+  const [enabled2fa, setEnabled2faa] = useState(twoFactor);
+  
+  const handle2fa = async () => {
+    try {
+      if (enabled2fa) {
+        await SettingsServices.update2fa(false);
+        setEnabled2faa(false);
+        setEnabled2fa(false);
+      } else {
+        setEnabled2faa(true);
+        setEnabled2fa(true);
+      }
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
     return (
-        <div className="w-full py-6 flex flex-wrap items-center justify-start h-fit gap-10 border-2 bg-[#00000099] rounded-[50px] shadow-2xl p-7 border-[#B8B8B8]">
+        <div className="w-full py-6 flex flex-wrap items-center justify-start h-fit gap-10">
         <div className="w-full h-[12%] flex items-center">
-          <h1 className="text-white font-dayson font-bold text-2xl tracking-wider hover:border-b-2 transition-all duration-200">
-            Two Factor Authentication
+            <h1 className="text-white font-dayson font-bold text-xl tracking-wider transition-all duration-200 opacity-[80%]">
+              Two Factor Authentication
           </h1>
         </div>
-        <div className="w-full h-[50%] 2xl:pl-16 xl:pl-10 flex items-start justify-between flex-col">
+        <div className="w-full h-[50%] flex items-start justify-between flex-col gap-4">
           <p className="font-coustard text-lg text-white opacity-[80%]">
             Two Factor Authentication protects your account by <br />
             requiring an additional code when you log in on a new device.<br />
           </p>
-          <div className="w-full h-[50%] flex items-center justify-start flex-row gap-5">
-            <h1 className="font-coustard text-xl text-white">Activate 2FA</h1>
-            <Switch />
-          </div>
+          <div className="w-full h-[50%] flex items-center justify-start flex-row gap-10">
+          <h1 className="font-coustard text-xl text-white">
+            {enabled2fa ? 'Enabled' : 'Disabled'}
+          </h1>
+            <Switch checked={enabled2fa} onCheckedChange={handle2fa}/>
+          </div> 
         </div>
       </div>
     )
--- src/Frontend/source/components/settings/Profile.tsx
+++ src/Frontend/source/components/settings/Profile.tsx
@@ -7,9 +7,22 @@ import { useUser } from '@/context/GlobalContext';
 import ProfileInformations from './ProfileInformayions';
 import { SecurityComponent } from './SecurityComponent';
 import SettingsServices from '@/services/settingsServices';
+import { IconLock } from '@tabler/icons-react';
+import { IconLockOpen2 } from '@tabler/icons-react';
+import { Switch } from '../ui/switch';
 
 export default function ProfileInfo() {
   const { user } = useUser();
+  const [privacy, setPrivacy] = useState(false);
+  const [enabled2fa, setEnabled2fa] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+  useEffect(() => {
+    const get2fa = async () => {
+      const response = await SettingsServices.get2fa();
+      setEnabled2fa(response);
+    }
+    get2fa();
+  }, []);
   if (!user) {
     return (
       <div className="w-full h-full flex items-center justify-center">
@@ -17,83 +30,109 @@ export default function ProfileInfo() {
       </div>
     );
   }
-  const [isClicked, setIsClicked] = useState(false);
-  const [profileState, setProfileState] = useState({
-    avatar: user?.avatar,
-    cover: user?.cover,
-    profileError: '',
-    coverError: '',
-    username: user?.username,
-    display_name: user?.display_name,
-    password: '',
-    NewPassword: '',
-    is_private: user?.is_private,
-  });
-  const [errors, setErrors] = useState<Record<string, string>>({});
-  const updateState = (key: keyof typeof profileState, value: any) => {
-    setProfileState((prev: any) => ({ ...prev, [key]: value }));
-  };
-  const handleClick = () => {
-    const Nameschema = z.object({
-      display_name: z.string().min(3, 'Display name must be at least 3 characters'),
-      password: z
-    .string()
-    .refine((val: any) => val === '' || val.length >= 6, {
-      message: 'Password must be at least 6 characters or empty',
-    }),
-  NewPassword: z
-    .string()
-    .refine((val: any) => val === '' || val.length >= 6, {
-      message: 'New Password must be at least 6 characters or empty',
-    }),
-    });
+  // const [profileState, setProfileState] = useState({
+  //   avatar: user?.avatar,
+  //   cover: user?.cover,
+  //   profileError: '',
+  //   coverError: '',
+  //   username: user?.username,
+  //   display_name: user?.display_name,
+  //   password: '',
+  //   NewPassword: '',
+  //   is_private: user?.is_private,
+  // });
+  // const [errors, setErrors] = useState<Record<string, string>>({});
+  // const handleClick = () => {
+  //   const Nameschema = z.object({
+  //     display_name: z.string().min(3, 'Display name must be at least 3 characters'),
+  //     password: z
+  //   .string()
+  //   .refine((val: any) => val === '' || val.length >= 6, {
+  //     message: 'Password must be at least 6 characters or empty',
+  //   }),
+  // NewPassword: z
+  //   .string()
+  //   .refine((val: any) => val === '' || val.length >= 6, {
+  //     message: 'New Password must be at least 6 characters or empty',
+  //   }),
+  //   });
 
-    const validationResult = Nameschema.safeParse({
-      display_name: profileState.display_name,
-      password: profileState.password,
-      NewPassword: profileState.NewPassword,
-    });
+  //   const validationResult = Nameschema.safeParse({
+  //     display_name: profileState.display_name,
+  //     password: profileState.password,
+  //     NewPassword: profileState.NewPassword,
+  //   });
 
-    if (!validationResult.success) {
-      const errorMap = validationResult.error.errors.reduce(
-        (acc: any, err: any) => {
-          acc[err.path[0]] = err.message;
-          return acc;
-        },
-        {} as Record<string, string>
-      );
-      setErrors(errorMap);
-      return;
-    } else {
-      setErrors({});
-      console.log(profileState);
-      const updatedProfileState = {
-        selectedImage: profileState.avatar,
-        coverImage: profileState.cover,
-        username: profileState.username,
-        password: profileState.password,
-        NewPassword: profileState.NewPassword,
-      };
-      setProfileState(updatedProfileState as any);
-      SettingsServices.updateSettings(updatedProfileState);
-      setIsClicked(true);
-      setTimeout(() => {
-        setIsClicked(false);
-      }, 4000);
-    }
+  //   if (!validationResult.success) {
+  //     const errorMap = validationResult.error.errors.reduce(
+  //       (acc: any, err: any) => {
+  //         acc[err.path[0]] = err.message;
+  //         return acc;
+  //       },
+  //       {} as Record<string, string>
+  //     );
+  //     setErrors(errorMap);
+  //     return;
+  //   } else {
+  //     setErrors({});
+  //     console.log(profileState);
+  //     const updatedProfileState = {
+  //       selectedImage: profileState.avatar,
+  //       coverImage: profileState.cover,
+  //       username: profileState.username,
+  //       password: profileState.password,
+  //       NewPassword: profileState.NewPassword,
+  //     };
+  //     setProfileState(updatedProfileState as any);
+  //     SettingsServices.updateSettings(updatedProfileState);
+  //     setIsClicked(true);
+  //     setTimeout(() => {
+  //       setIsClicked(false);
+  //     }, 4000);
+  //   }
+  // };
+
+  const handlePrivacy = async () => {
+    const newPrivacyState = !privacy;
+    setPrivacy(newPrivacyState);
+    await SettingsServices.updatePrivacy(newPrivacyState);
   };
   return (
-    <div className="size-full py-8 px-6 2xl:p-10 flex flex-col gap-8">
-      <ProfileInformations profileState={profileState} setProfileState={setProfileState} errors={errors} setErrors={setErrors} />
-      <SecurityComponent  profileState={profileState} setProfileState={setProfileState} errors={errors} setErrors={setErrors}/>
-      <Activate_2fa  profileState={profileState} setProfileState={setProfileState}/>
-      <div className="w-[30%] h-[5%] flex items-center justify-center">
+    <div className="size-full md:py-8 md:px-6 2xl:p-10 flex flex-col md:gap-8">
+      <ProfileInformations />
+      <div className="w-full h-fit flex flex-col gap-16 border-2 bg-[#00000099] md:rounded-[50px] shadow-2xl p-7 border-[#B8B8B8]">
+        <div className="w-full h-[8%] flex items-center">
+        <h1 className="text-white font-dayson font-bold text-2xl tracking-wider border-b-2 transition-all duration-200">
+          Profile Privacy
+        </h1>
+      </div>
+        <div className=" 2xl:px-20 py-6 w-full h-fit flex flex-row gap-9">
+        {privacy ? <IconLock stroke={1.75} className="text-white"/> : <IconLockOpen2 stroke={1.75} className="text-white"/>}
+        <h1 className="font-coustard text-xl text-white">Private Profile</h1>
+        <Switch checked={privacy} onCheckedChange={handlePrivacy}/>
+        </div>
+        {/* <div className="w-full h-[5%] flex items-center justify-end">
         <button
           className={`${isClicked ? 'bg-green-500' : 'bg-white hover:bg-[#28AFB0]'} w-[200px] h-[50px] py-3 px-6 text-black font-dayson rounded-md font-bold text-lg hover:bg-opacity-[90%] transition-all duration-200`}
           onClick={handleClick}
         >
           {isClicked ? 'Updated' : 'Update'}
         </button>
+      </div> */}
+      </div>
+      <div className="w-full h-fit flex flex-col gap-4 border-2 bg-[#00000099] md:rounded-[50px] shadow-2xl p-7 border-[#B8B8B8]">
+        <div className="w-full h-fit flex flex-row gap-4">
+      <SecurityComponent />
+      {/* <Activate_2fa enabled2fa={enabled2fa} /> */}
+      </div>
+      {/* <div className="w-[30%] h-[5%] flex items-center justify-end">
+        <button
+          className={`${isClicked ? 'bg-green-500' : 'bg-white hover:bg-[#28AFB0]'} w-[250px] h-[50px] py-3 px-6 text-black font-dayson rounded-md font-bold text-lg hover:bg-opacity-[90%] transition-all duration-200`}
+          // onClick={handleClick}
+        >
+          {isClicked ? 'Updated' : 'Update'}
+        </button>
+      </div> */}
       </div>
     </div>
   );
