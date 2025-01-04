dba69438e42751219d7fce315b20aafc6a056170
AbdelkaderAoutem <abdlkadraoutem@gmail.com>
2025-01-04 17:50:30 +0100

    On game

--- src/Backend/source/game/GameEngine.py
+++ src/Backend/source/game/GameEngine.py
@@ -27,25 +27,22 @@ class Ball:
     position: Vector2D
     velocity: Vector2D
     radius: float = 2.0
-    
+
     async def update(self, delta_time: float):
         self.position = self.position + (self.velocity * delta_time)
 
     async def reset(self):
         self.position = Vector2D(37.5, 50)
-        self.velocity = Vector2D(30, 30)# Pixels per second
+        self.velocity *= -1
 
 @dataclass 
 class Paddle:
     position: Vector2D
-    width: float = 14.0 #15.0 
+    width: float = 9.0
     height: float = 2.5
 
     def move(self, new_x: float):
-
-        # self.position.x = new_x
-        self.position.x = max(0, min(75 - self.width , new_x))
-        # print (f'{YELLOW}NEW  position: {self.position.x}\033[0m')
+        self.position.x = new_x + 7.5
 
 from typing import List
 class GamePlayer:
@@ -56,6 +53,7 @@ class GamePlayer:
         self.channel_name = channel_name
         self.status = 'waiting'# waiting, ready, playing, paused, 'finished'
         self.paddle = None
+        self.map = 'air' # 'air','water','fire','earth'
         self.score: List[int] = [0,0,0]
 
 class PingPongGame:
@@ -66,18 +64,19 @@ class PingPongGame:
         self.game_width = 75
         self.game_height = 100
         self.status = 'waiting'  # waiting, start, pause, over
-        self.ball = Ball(Vector2D(37.5, 50), Vector2D(30, 30)) 
+        self.ball = Ball(Vector2D(37.5, 50), Vector2D(35, 35))
 
         self.player1 = player1
         self.player1.game_id = gameID
-        self.player1.paddle = Paddle(Vector2D(37.5, 1.5))  # Lower paddle 
+        self.player1.paddle = Paddle(Vector2D(37.5, 4.5))  # Lower paddle 
         self.player2 = player2
         self.player2.game_id = gameID
-        self.player2.paddle = Paddle(Vector2D(37.5, 98.5))  # Upper paddle
+        self.player2.paddle = Paddle(Vector2D(37.5, 96.5))  # Upper paddle
 
         self.winner = None
         self.round = 0
-        self.winning_score = 21
+        self.map = 'water' # 'air','water','fire','earth'
+        # self.new
 
     def start_game(self):
         import time
@@ -92,20 +91,19 @@ class PingPongGame:
         if self.status != 'playing':
             return False 
         
-        # implement game rounds and pause game3 round 7 goals
+        # implement game rounds and pause game, 3 round 7 goals
         await self.ball.update(delta_time)
 
+        # if await self.check_game_end():
+        #     return False 
         if await self.check_collisions():
             return True
-        if await self.check_scoring():
-            return True
-        if await self.check_for_rounds():
-            return True
-        if await self.check_game_end():
-            return True 
+        # if await self.check_scoring():
+        #     return False
+        # if await self.check_for_rounds():
+        #     return True
         return False
 
- 
     async def check_collisions(self) -> bool:
         changed = False
 
@@ -116,30 +114,23 @@ class PingPongGame:
 
         if await self.check_paddle_collision(self.player1.paddle):
             self.ball.velocity.y = abs(self.ball.velocity.y)  # Move right
-            await self.adjust_ball_angle(self.player1.paddle) 
+            # await self.adjust_ball_angle(self.player1.paddle) 
             changed = True
         elif await self.check_paddle_collision(self.player2.paddle):
             self.ball.velocity.y = -abs(self.ball.velocity.y)  # Move left
-            await self.adjust_ball_angle(self.player2.paddle)
+            # await self.adjust_ball_angle(self.player2.paddle) 
             changed = True
         return changed
 
     async def check_paddle_collision(self, paddle: Paddle) -> bool:
 
-        collosion_x = abs(self.ball.position.x - paddle.position.x) < (paddle.width + self.ball.radius)
-        collosion_y = abs(self.ball.position.y - paddle.position.y) < (paddle.height + self.ball.radius)
+        collosion_x = abs(self.ball.position.x - paddle.position.x) < (paddle.width)
+        collosion_y = abs(self.ball.position.y - paddle.position.y) < (paddle.height)
+        if collosion_x and collosion_y:
+            print(f'{GREEN}paddle position {paddle.position.x} and {self.ball.position.x}\033[0m')
+            print(f'{GREEN}Collision detected with {abs(self.ball.position.x - paddle.position.x)} and {paddle.width + self.ball.radius}\033[0m')
         return collosion_x and collosion_y
 
-    # async def _adjust_ball_angle(self, paddle: Paddle):
-    #     """Adjust ball angle based on where it hits the paddle"""
-    #     relative_intersect_x = (paddle.position.x - self.ball.position.x)
-    #     normalized_intersect = relative_intersect_x / (paddle.height / 2)
-    #     bounce_angle = normalized_intersect * math.pi / 4  # 45 degrees max angle
-
-    #     speed = math.sqrt(self.ball.velocity.x**2 + self.ball.velocity.y**2)
-    #     self.ball.velocity.x = speed * math.cos(bounce_angle)
-    #     self.ball.velocity.y = speed * math.sin(bounce_angle)
-        
     async def adjust_ball_angle(self, paddle: Paddle):
         import math
 
@@ -147,32 +138,32 @@ class PingPongGame:
         normalized_collision = (collisionPoint_x - paddle.width/2) / (paddle.width / 2)
         self.ball.velocity.x = normalized_collision * (abs(normalized_collision) + 0.5)
 
-        # speed = math.sqrt(self.ball.velocity.x**2 + self.ball.velocity.y**2)
-
-        # Assume a 'bottom' paddle has a lower y-value and a 'top' paddle has a higher y-value
-        # if paddle.position.y < (self.game_height / 2):
-        #     # Bounce the ball upward
-        #     self.ball.velocity.x = speed * math.cos(bounce_angle)
-        #     self.ball.velocity.y = abs(speed * math.sin(bounce_angle))
-        # else:
-        #     # Bounce the ball downward
-        #     self.ball.velocity.x = speed * math.cos(bounce_angle)
-        #     self.ball.velocity.y = -abs(speed * math.sin(bounce_angle))
-
     async def check_for_rounds(self) :
-        if self.round == 3:
-            #TODO broadcast game end
-            # declare a winner and end the game
-            return False
-        if (self.player1.score[self.round] == 7 or self.player2.score[self.round] == 7):
+        # TODO if (self.player1.score[self.round] == 7 or self.player2.score[self.round] == 7):
+        if (self.player1.score[self.round] == 1 or self.player2.score[self.round] == 1):
             self.round += 1
+            #TODO to check and test
+            if self.round == 3:
+                self.status = 'finished'
+                if sum(self.player1.score) > sum(self.player2.score) :
+                    self.winner = 'player1'
+                    self.map = self.player1.map
+                elif sum(self.player1.score) == sum(self.player2.score):
+                    self.winner = 'draw'
+                    self.map = self.player1.map
+                else :
+                    self.winner = 'player2'
+                    self.map = self.player2.map
+                return True
             #TODO broadcast round end
+            print (f'{YELLOW}Round {self.round}, score: {self.player1.score}, {self.player2.score}{RESET}')
             await asyncio.sleep(5)
             return True
         return False
     async def check_scoring(self) -> bool: 
         if self.ball.position.y <= 0: # Player 2 scores
             self.player2.score[self.round] += 1
+            #TODO reset paddle position
             await self.ball.reset()
             return True
         elif self.ball.position.y >= 100:  # Player 1 scores
@@ -181,18 +172,8 @@ class PingPongGame:
             return True
         return False
 
-    async def check_game_end(self) -> bool:
-        if sum(self.player1.score) >= self.winning_score or \
-           sum(self.player2.score) >= self.winning_score:
-            self.status = 'finished'
-            self.winner = self.player1 if sum(self.player1.score) > sum(self.player2.score) else self.player2
-            return True
-        return False
-
     #TODO to be fixed
     async def move_paddle(self, player_id: str, new_x: float) -> bool:
-        # print(f'{BLUE}Moving paddle to {new_x}\033[0m')
-        # print(f'{BLUE} self:{player_id} Player1: {self.player1.id} Player2: {self.player2.id}\033[0m')
         if player_id == self.player1.id:
             self.player1.paddle.move(new_x)
             return True
@@ -201,28 +182,28 @@ class PingPongGame:
             return True
         return False
      
+    async def check_game_end(self) -> bool:
+        if self.round == 3:
+            self.status = 'finished'
+            if sum(self.player1.score) > sum(self.player2.score) :
+                self.winner = 'player1'
+                self.map = self.player1.map
+            elif sum(self.player1.score) == sum(self.player2.score):
+                self.winner = 'draw'
+                self.map = self.player1.map
+            else :
+                self.winner = 'player2'
+                self.map = self.player2.map
+            return True
+        return False
+
     async def ball_update(self) -> Dict:
-        
         return {
             'dx': self.ball.velocity.x,
             'dy': self.ball.velocity.y,
             'position':
             {
-                'x': self.ball.position.x, # / self.game_width,
-                'y': self.ball.position.y # / self.game_height
+                'x': self.ball.position.x,
+                'y': self.ball.position.y
             },
         }
-
-    def paddle_update(self, opponent_id: int) -> Dict:
-        player = self.player1 if self.player1.id == opponent_id else self.player2
-        return {
-            self.player.username: {
-            {
-                'paddle_x': self.player1.paddle.position.x, # / self.game_height,
-            },
-            }}
-            # self.player2.username: {
-            #     'paddle_x': self.player2.paddle.position.x, # / self.game_height,
-            # },
-            # }
-
--- src/Backend/source/game/Gameconsumer0.py
+++ src/Backend/source/game/Gameconsumer0.py
@@ -5,13 +5,16 @@ from channels.db import database_sync_to_async
 from .MatchMaking import MatchMakingSystem
 from accounts.models import Player, PlayerProfile
 from game.models import Game
-
+from accounts.models import MatchHistory
 matchmake_system = MatchMakingSystem()
 
 RED = '\033[31m'
 GREEN = '\033[32m'
 YELLOW = '\033[33m'
 BLUE = '\033[34m'
+VIOLET = '\033[35m'
+ORANGE = '\033[36m'
+PINK = '\033[37m'
 RESET = '\033[0m'
 #TODO handle the unexpected disconnects and game state details
 
@@ -35,19 +38,19 @@ class GameConsumer(AsyncWebsocketConsumer):
         if not matchmake_system._running:
             await matchmake_system.start()
         #TODO to fix 
-        # self.profile.status = 'waiting'
-        # await database_sync_to_async(self.profile.save)()
+        self.profile.status = 'waiting'
+        await database_sync_to_async(self.profile.save)(update_fields=['status'])
 
         await self.accept()
 
-        if not self.player_in_QG():
+        if (self.profile.status == 'waiting') and not self.player_in_QG():
             print(f'\n{YELLOW}[Adding Player to Queue]{RESET}\n')
             await matchmake_system.add_player_to_queue(self.user.id, self.user.username, self.channel_name)
         else:
             print(f'\n{YELLOW}[User Already in a Game]{RESET}\n')
 
     def player_in_QG(self):
-        return (self.user.id in matchmake_system.players_in_game) or (self.user.id in matchmake_system.players_queue)
+        return (self.user.id in matchmake_system.players_in_game) and (self.user.id in matchmake_system.players_queue)
 
     async def receive(self, text_data):
         try:
@@ -56,6 +59,7 @@ class GameConsumer(AsyncWebsocketConsumer):
             print(f'\n{YELLOW}data: [{data}]{RESET}\n')
 
             if action == 'ready':
+                print(f'\n{YELLOW}[Player ReadyYYYYYYYYYY]{RESET}\n')
                 game_id = data.get('game_id', None)
                 if not game_id :
                     return
@@ -65,6 +69,7 @@ class GameConsumer(AsyncWebsocketConsumer):
                     return
                 await self.set_Gameplayers_to_consumer()
                 self.Gameplayer.status = 'ready'
+                #TODO Include the game map in the ready event
                 self.broadcast_ready()
                 #TODO Am not sure if we still need this sleep for synchronization or not[CHECK with the Noredin]
                 await asyncio.sleep(2)
@@ -96,15 +101,19 @@ class GameConsumer(AsyncWebsocketConsumer):
                 delta_time = 0.016 #60 FPS
                 if await self.game.update(delta_time):
                     await self.broadcast_ball_move()
-                    print(f'\n{YELLOW}players score : [{self.game.player1.score}, {self.game.player2.score}]{RESET}\n')
+                if await self.game.check_scoring():
+                    await self.broadcast_score_update()
+                if await self.game.check_for_rounds():
+                    await self.broadcast_score_update()
+                    # await self.broadcast_end_round()#TODO to implement
                 await asyncio.sleep(delta_time)
             if self.game and self.game.status == 'finished':
-                await self.broadcast_ball_move()
+                print(f'\n{YELLOW}[Game Finished]{RESET}\n')
                 await self.handle_game_end()
         except Exception as e:
-            print(f'\n{RED}[Error in Game Loop {str(e)}]{RESET}\n')
-        finally :
-            self.game_tick = None
+            print(f'\n{RED}[Error in Game Loop {str(e)} {self.game.round}]{RESET}\n')
+        # finally :
+        #     self.game_tick = None
 
     #------------------------------------>>>>>broadcast Events<<<<<<<------------------------------
     async def game_found(self, event):
@@ -118,6 +127,35 @@ class GameConsumer(AsyncWebsocketConsumer):
                     'game_id': event['game_id']
                 }
             }))
+
+    # async def update_score(self, event):
+    #     await self.send(text_data=json.dumps({
+    #         'type': 'UpdateScore',
+    #         'round': event['round'],
+    #         'self_score': event['self_score'],
+    #         'opponent_score': event['opponent_score']
+    #     }))
+    # async def broadcast_score_update(self):
+    #     if not self.game:
+    #         return
+    #     await self.channel_layer.send(
+    #         self.opponent.channel_name,
+    #         {
+    #             'type': 'update_score',
+    #             'round': self.game.round,
+    #             'self_score': self.opponent.score,
+    #             'opponent_score': self.Gameplayer.score
+    #         }
+    #     )
+    #     await self.channel_layer.send(
+    #         self.Gameplayer.channel_name,
+    #         {
+    #             'type': 'update_score',
+    #             'round': self.game.round,
+    #             'self_score': self.Gameplayer.score,
+    #             'opponent_score': self.opponent.score
+    #         }
+    #     )
 
     async def paddle_move(self, event):
         await self.send(text_data=json.dumps({
@@ -156,68 +194,161 @@ class GameConsumer(AsyncWebsocketConsumer):
             self.opponent = self.game.player2 
             self.Gameplayer = self.game.player1
 
-    async def broadcast_ready(self): # broadcast ready to players then start the game
-        await matchmake_system.channel_layer.send(
-            self.game.player1.channel_name,
-            {
-                'type': 'acknowledge.ready',
-                'state': "start"
-            }
-        )
-        await matchmake_system.channel_layer.send(
-            self.game.player2.channel_name,
-            {
-                'type': 'acknowledge.ready',
-                'state': "start"
-            }
-        )
-
     async def acknowledge_ready(self, event):
         await self.send(text_data=json.dumps({
             'type': 'gameState',
             'state': event['state']
         }))
+    async def broadcast_ready(self):# broadcast ready to players then start the game
+        await matchmake_system.channel_layer.group_send(
+            f'game_{self.game_id}',
+            {
+                'type': 'acknowledge.ready',
+                'state': 'start'
+            })
 
     async def handle_game_end(self):
         if not self.game:
             return
         try:
             print (f'\n{RED}[Game End {self.game_id}]{RESET}\n')
-            self.game.status = 'finished'
-            del matchmake_system.games[self.game_id]
+            await self.save_game_result(self.game.player1.id, self.game.player2.id, self.game.winner)
             matchmake_system.players_in_game.remove(self.user.id)
+            matchmake_system.players_in_game.remove(self.opponent.id)
+            if self.game_id in matchmake_system.games:
+                del matchmake_system.games[self.game_id]
+            await self.getBack_To_WaitingStatus()
+
         except Exception as e:
+            #TODO self.close()
             print(f'\n{RED}[Error in Game End {str(e)}]{RESET}\n')
-        self.game = None
-        self.game_id = None
+        finally:
+            await self.broadcast_game_end()
+            await self.disconnect(1000)
+            # await self.close()
+            self.game = None
+            self.game_id = None
 
     @database_sync_to_async
-    def save_game_result(self, game_state):
-        pass
+    def save_game_result(self, player1_id, player2_id, result_value):
+        
+        player1_m = Player.objects.select_related('profile').get(id=player1_id)
+        player2_m = Player.objects.select_related('profile').get(id=player2_id)
+        MatchHistory.objects.create(
+            player1=player1_m.profile,
+            player2=player2_m.profile,
+            player1_score=sum(self.game.player1.score),
+            player2_score=sum(self.game.player2.score),
+            result=str(result_value),
+            map_played=self.game.map
+        )
 
-    # Implement game result saving logic here
+    async def unexpected_disconnect(self, player1_id, player2_id, result_value):
+        print(f'\n{RED}[Unexpected Disconnection]{RESET}\n')
+        await self.save_game_result(player1_id, player2_id, result_value)
+
+        matchmake_system.players_in_game.remove(self.user.id)
+        matchmake_system.players_in_game.remove(self.opponent.id)
+
+    async def game_over(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'GameEnd',
+            'winner': event['winner']
+        }))
+    #TODO 'winner': self.game.winner to be altered to username or smthg else
+    async def broadcast_game_end(self):
+        await matchmake_system.channel_layer.group_send(
+            f'game_{self.game_id}',
+            {
+                'type': 'game_over',
+                'winner': self.game.winner
+            })
+
     async def disconnect(self, close_code):
     # TODO handle the unexpeted disconnects or cleanup after normal disconnect
-        if self.game:
-            self.game.status = 'finished'
-            self.game.player1.status = 'waiting'
-            self.game.player2.status = 'waiting'
-            del matchmake_system.games[self.game_id]
-            self.channel_layer.send(
-                self.opponent.channel_name,
+        try:
+            if self.game:
+                print(f'\n{RED}[Disconnect {self.user.username} AMAMAMAMM HERERERER]{RESET}\n')
+                await self.getBack_To_WaitingStatus()
+                # await self.broadcast_game_end()
+                if self.game.status == 'playing':
+                    await self.unexpected_disconnect(self.user.id, self.opponent.id, 'player2')
+                    await self.broadcast_disconnection()
+                if self.game_id in matchmake_system.games:
+                    del matchmake_system.games[self.game_id]
+                print(f'\n{RED}[Disconnect {self.user.username}]{RESET}\n')
+            await self.channel_layer.group_discard(f'game_{self.game_id}', self.channel_name)
+
+            await self.close()
+        except Exception as e:
+            print(f'\n{RED}[Error in Disconnect {str(e)}]{RESET}\n')
+
+    #force disconnection from both sides
+    async def broadcast_disconnection(self):
+        await matchmake_system.channel_layer.group_send(
+            f'game_{self.game_id}',
+            {
+                'type': 'force_disconnect',
+                'winner': 'draw'
+            })
+    async def force_disconnect(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'GameEnd',
+            'winner': event['winner']
+        }))
+
+    #------------------------------------>>>>>util functions<<<<<<<------------------------------
+    async def getBack_To_WaitingStatus(self):
+        self.profile.status = 'waiting'
+        await database_sync_to_async(self.profile.save)(update_fields=['status'])
+    #------------------------------------>>>>>util functions<<<<<<<------------------------------
+    async def update_score(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'UpdateScore',
+            'data': event['data']
+        }))
+
+    async def broadcast_score_update(self):
+        if not self.game:
+            return
+        try:
+            await self.channel_layer.send(
+                self.Gameplayer.channel_name,
                 {
-                    'type': 'game_end',
+                    'type': 'update_score',
+                    'data':{
+                        'round': self.game.round,
+                        'self_score': self.Gameplayer.score,
+                        'opponent_score': self.opponent.score
+                    }
                 }
             )
-            #TODO update PlayerProfile
-            print(f'\n{RED}[Disconnect {self.user.username}]{RESET}\n')
-        await self.close()
+            await self.channel_layer.send(
+                self.opponent.channel_name,
+                {
+                    'type': 'update_score',
+                    'data':{
+                        'round': self.game.round,
+                        'self_score': self.opponent.score,
+                        'opponent_score': self.Gameplayer.score
+                    }
+                }
+            )
+        except Exception as e:
+            print(f'\n{RED}[Error in Score Update {str(e)}]{RESET}\n')
 
-    async def game_end(self, event):
-        print(f'disconnect {self.user.username}')
-        self.close()
 
 
-#TODO handle asyncrounous ball movs [DONE]?????????????
-#TODO handle the paddle movs [About to be done]
+
+#TODO check with nouredine to send the game map in ready event 
+#TODO handle the paddle movs [About to be done] {NEED to implement the paddle effect}!!!!!!!!
+#TODO handle the force disconnect if unexpected disconnect 
+#TODO handle the game end and Score 
 #TODO handle the game end and disconnections
+#TODO handle the game maps
+#TODO broadcast round end
+#TODO Why the ball updates after the round stop
+
+
+#_______________________________Sbject requirements _______________________________________
+# 1- the app should be protected against XSS attacks
