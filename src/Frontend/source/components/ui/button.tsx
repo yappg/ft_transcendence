587e720a2e5c7f8f2cfda0c8295022acccdc0d7c
Meryeme Mesbahi <mmesbahi@e3r3p12.1337.ma>
2025-01-06 09:04:57 +0100

    building

--- src/Frontend/source/components/ui/button.tsx
+++ src/Frontend/source/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
-        "text-destructive-foreground bg-destructive hover:bg-destructive/90",
+          "text-destructive-foreground bg-destructive hover:bg-destructive/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
