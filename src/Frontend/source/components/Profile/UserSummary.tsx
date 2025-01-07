f35c1566bf9163aca9809cf88a4774c75429ba1a
ElmehdiBennix <bennixmehdi@gmail.com>
2025-01-07 03:30:03 +0100

    added skeletons to profile

--- src/Frontend/source/components/Profile/UserSummary.tsx
+++ src/Frontend/source/components/Profile/UserSummary.tsx
@@ -19,34 +19,37 @@ const UserSummary = ({
 }): JSX.Element => {
   if (!user) return <div>Loading...</div>;
   const { total_games, achievements } = user;
-  return (
-    <div className="flex h-fit w-full flex-col items-center overflow-x-hidden overflow-y-scroll bg-[#242627]/90 scrollbar-hide lg:h-full lg:flex-row">
-      <div className="z-50 flex h-fit w-full flex-col items-center justify-center gap-2 lg:h-full lg:w-[63%]">
-        <Achievments achievements={achievements} />
-        {!is_private ? (
+  console.log("is_private: ", is_private);
+  if (is_private) {
+    return (
+      <div className="flex size-full items-center justify-center text-black-crd dark:text-white-crd">
+        This profile is private
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex h-fit w-full flex-col items-center overflow-x-hidden overflow-y-scroll bg-[#242627]/90 scrollbar-hide lg:h-full lg:flex-row">
+        <div className="z-50 flex h-fit w-full flex-col items-center justify-center gap-2 lg:h-full lg:w-[63%]">
+          <Achievments achievements={achievements} />
           <div className="flex h-fit w-full flex-col items-center justify-start gap-9 lg:h-full lg:flex-row lg:px-7 xl:py-5 2xl:gap-12 2xl:py-10">
             <MatchHistory PlayerMatches={user.matches_history} />
             <Friends players={user.friends} />
           </div>
-        ) : (
-          <div className="flex h-fit w-full flex-col items-center justify-start gap-4 py-2 lg:h-[calc(100%-100px)] lg:flex-row lg:justify-between">
-            this profile is private
-          </div>
-        )}
-      </div>
-      <div className="z-50 m-8 flex size-full flex-col items-center justify-center gap-10 rounded-[14px] bg-[#4C4D4E] shadow-2xl md:gap-2 lg:h-[90%] lg:w-[37%] lg:rounded-[30px]">
-        <div className="flex h-1/2 w-full flex-col items-center justify-center gap-20 sm:gap-2 xl:flex-row">
-          <div className="h-1/2 w-full sm:h-full sm:w-1/2">
-            <Chart total_games={total_games} stats={user?.statistics} />
-          </div>
-          <Rating statistics={user?.statistics} />
         </div>
-        <div className="flex h-1/2 w-full items-center justify-center">
-          <ChartLine statistics={user?.statistics} />
+        <div className="z-50 m-8 flex size-full flex-col items-center justify-center gap-10 rounded-[14px] bg-[#4C4D4E] shadow-2xl md:gap-2 lg:h-[90%] lg:w-[37%] lg:rounded-[30px]">
+          <div className="flex h-1/2 w-full flex-col items-center justify-center gap-20 sm:gap-2 xl:flex-row">
+            <div className="h-1/2 w-full sm:h-full sm:w-1/2">
+              <Chart total_games={total_games} stats={user?.statistics} />
+            </div>
+            <Rating statistics={user?.statistics} />
+          </div>
+          <div className="flex h-1/2 w-full items-center justify-center">
+            <ChartLine statistics={user?.statistics} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default UserSummary;
